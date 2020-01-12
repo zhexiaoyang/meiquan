e77c0ee5dad34bc6862eaf9db7a6bc7f8ec32231
zhangzhen <zhangzhen@oneweone.cn>
2020-01-13 02:29:03 +0800

    接入美全订单

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -24,7 +24,7 @@ class Order extends Model
         'goods_height','goods_width','goods_length','goods_weight','goods_pickup_info',
         'goods_delivery_info','expected_pickup_time','expected_delivery_time','order_type','poi_seq',
         'note','cash_on_delivery','cash_on_pickup','invoice_title','mt_peisong_id', 'courier_name',
-        'courier_phone', 'cancel_reason_id', 'cancel_reason','status','failed'];
+        'courier_phone', 'cancel_reason_id', 'cancel_reason','status','failed','type'];
 
     public function shop() {
         return $this->belongsTo(Shop::class);
