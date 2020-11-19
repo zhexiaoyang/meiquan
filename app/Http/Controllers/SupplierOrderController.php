a83665580f7c9298c8c315a719ec80152ea8966f
zhangzhen <zhangzhen@yeah.net>
2020-11-19 13:13:44 +0800

    采购商城修改

--- app/Http/Controllers/SupplierOrderController.php
+++ app/Http/Controllers/SupplierOrderController.php
@@ -195,7 +195,7 @@ class SupplierOrderController extends Controller
                 $product_ids = collect($carts)->pluck('id');
                 $user->carts()->whereIn('id', $product_ids)->delete();
 
-                dispatch(new CloseOrder($order, 120));
+                dispatch(new CloseOrder($order, 600));
             }
 
             return $order;
