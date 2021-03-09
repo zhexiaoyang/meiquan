0d27cae6f080211c71d4de4aa85923d6b33087df
zhangzhen <zhangzhen@yeah.net>
2021-03-09 08:52:38 +0800

    功能修改

--- app/Http/Controllers/Supplier/UserController.php
+++ app/Http/Controllers/Supplier/UserController.php
@@ -185,6 +185,134 @@ class UserController extends Controller
     public function update(Request $request)
     {
         \Log::info("[采购端]-[采购用户]-[修改资质]-全部参数", $request->all());
+        $user = Auth::user();
+
+        if ($avatar = $request->get("avatar")) {
+            $user->avatar = $avatar;
+        }
+
+        if ($name = $request->get("name")) {
+            $user->name = $name;
+        }
+
+        if ($telephone = $request->get("telephone")) {
+            $user->telephone = $telephone;
+        }
+
+        if ($starting = intval($request->get("starting"))) {
+            $user->starting = $starting;
+        }
+
+        if ($yyzz = $request->get("yyzz")) {
+            $user->yyzz = $yyzz;
+            $user->auth_at = date("Y-m-d H:i:s");
+        }
+
+        if ($ypjy = $request->get("ypjy")) {
+            $user->ypjy = $ypjy;
+        }
+
+        if ($spjy = $request->get("spjy")) {
+            $user->spjy = $spjy;
+        }
+
+        if ($ylqx = $request->get("ylqx")) {
+            $user->ylqx = $ylqx;
+        }
+
+        if ($ndbg = $request->get("ndbg")) {
+            $user->ndbg = $ndbg;
+        }
+
+        if ($elqx = $request->get("elqx")) {
+            $user->elqx = $elqx;
+        }
+
+        if ($khxk = $request->get("khxk")) {
+            $user->khxk = $khxk;
+        }
+
+        if ($nsrdj = $request->get("nsrdj")) {
+            $user->nsrdj = $nsrdj;
+        }
+
+        if ($hggh = $request->get("hggh")) {
+            $user->hggh = $hggh;
+        }
+
+        if ($qygl = $request->get("qygl")) {
+            $user->qygl = $qygl;
+        }
+
+        if ($kpxx = $request->get("kpxx")) {
+            $user->kpxx = $kpxx;
+        }
+
+        if ($ypgxht = $request->get("ypgxht")) {
+            $user->ypgxht = $ypgxht;
+        }
+
+        if ($zlbzxys = $request->get("zlbzxys")) {
+            $user->zlbzxys = $zlbzxys;
+        }
+
+        if ($xssqwts = $request->get("xssqwts")) {
+            $user->xssqwts = $xssqwts;
+        }
+
+        if ($yzymbab = $request->get("yzymbab")) {
+            $user->yzymbab = $yzymbab;
+        }
+
+        if ($shtxd = $request->get("shtxd")) {
+            $user->shtxd = $shtxd;
+        }
+
+        if ($yyzz_start_time = $request->get("yyzz_start_time")) {
+            $user->yyzz_start_time = $yyzz_start_time;
+        }
+
+        if ($yyzz_end_time = $request->get("yyzz_end_time")) {
+            $user->yyzz_end_time = $yyzz_end_time;
+        }
+
+        if ($ypjy_start_time = $request->get("ypjy_start_time")) {
+            $user->ypjy_start_time = $ypjy_start_time;
+        }
+
+        if ($ypjy_end_time = $request->get("ypjy_end_time")) {
+            $user->ypjy_end_time = $ypjy_end_time;
+        }
+
+        if ($spjy_start_time = $request->get("spjy_start_time")) {
+            $user->spjy_start_time = $spjy_start_time;
+        }
+
+        if ($spjy_end_time = $request->get("spjy_end_time")) {
+            $user->spjy_end_time = $spjy_end_time;
+        }
+
+        if ($ylqx_start_time = $request->get("ylqx_start_time")) {
+            $user->ylqx_start_time = $ylqx_start_time;
+        }
+
+        if ($ylqx_end_time = $request->get("ylqx_end_time")) {
+            $user->ylqx_end_time = $ylqx_end_time;
+        }
+
+        if ($description = $request->get("description")) {
+            $user->description = $description;
+        }
+
+        if ($notice = $request->get("notice")) {
+            $user->notice = $notice;
+        }
+
+        if ($chang = $request->get("chang")) {
+            $user->chang = $chang;
+        }
+
+        $user->save();
         return $this->success();
     }
 }
