25d273cdb4678f797764ca07d4cb887666e410a4
zhangzhen <zhangzhen@yeah.net>
2021-03-15 21:10:18 +0800

    对接美全服务商

--- app/Http/Controllers/Api/Waimai/MeiTuanMeiquanController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanMeiquanController.php
@@ -16,32 +16,32 @@ class MeiTuanMeiquanController extends Controller
     public function pay(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送已支付订单回调URL]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
     public function create(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送已确认订单回调]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
     public function cancel(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送用户或客服取消订单回调]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
     public function refund(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送全额退款信息回调]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
     public function refundPart(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送部分退款信息回调]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
     public function logistics(Request $request)
     {
         \Log::info("[外卖-美团服务商]-[推送美配订单配送状态回调]-全部参数：", $request->all());
-        return 200;
+        return json_encode(['data' => 'ok']);
     }
 
     // public function test(Request $request)
@@ -51,18 +51,24 @@ class MeiTuanMeiquanController extends Controller
     //     $meiquan = app("meiquan");
     //     $res = '';
     //
+    //     // $res = $meiquan->waimaiAuthorize(['response_type' => 'token','app_poi_code' => '6167_2705857']);
+    //     // return $res;
+    //
+    //
+    //
+    //
     //     if ($type == 1) {
-    //         $res = $meiquan->waimaiOrderConfirm(['order_id' => $id]);
+    //         $res = $meiquan->waimaiOrderConfirm(['order_id' => $id,'access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     } elseif ($type == 2) {
-    //         $res = $meiquan->waimaiOrderCancel(['order_id' => $id]);
+    //         $res = $meiquan->waimaiOrderCancel(['order_id' => $id,'access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     } elseif ($type == 3) {
-    //         $res = $meiquan->waimaiOrderRefundAgree(['order_id' => $id, 'reason' => '同意退款']);
+    //         $res = $meiquan->waimaiOrderRefundAgree(['order_id' => $id, 'reason' => '同意退款','access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     } elseif ($type == 4) {
-    //         $res = $meiquan->waimaiOrderRefundReject(['order_id' => $id, 'reason' => '拒绝退款']);
+    //         $res = $meiquan->waimaiOrderRefundReject(['order_id' => $id, 'reason' => '拒绝退款','access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     } elseif ($type == 5) {
-    //         $res = $meiquan->waimaiOrderBatchPullPhoneNumber(['order_id' => $id, 'offset' => 0, 'limit' => 100]);
+    //         $res = $meiquan->waimaiOrderBatchPullPhoneNumber(['order_id' => $id, 'offset' => 0, 'limit' => 100,'access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     } elseif ($type == 6) {
-    //         $res = $meiquan->waimaiOrderReviewAfterSales(['wm_order_id_view' => $id, 'review_type' => 1]);
+    //         $res = $meiquan->waimaiOrderReviewAfterSales(['wm_order_id_view' => $id, 'review_type' => 1,'access_token' => 'token_nZ-Mtc7LVt_RbVaW-2hJ5g','app_poi_code' => '6167_2705857']);
     //     }
     //
     //     return $res;
