37f6b3cad72d808138ca8d1c2921efaf2735c8ae
zhangzhen <zhangzhen@yeah.net>
2021-03-18 12:39:06 +0800

    财务更新

--- app/Libraries/Meituan/Api/Api.php
+++ app/Libraries/Meituan/Api/Api.php
@@ -33,6 +33,14 @@ class Api extends Request
             'goods_pickup_info' => $order->goods_pickup_info ? "取货码：" . $order->goods_pickup_info : ''
         ];
 
+        if ($order->day_seq) {
+            $params['poi_seq'] = $order->day_seq;
+            if ($order->platform) {
+                $params['platform'] = 101;
+                $params['outer_order_source_desc'] = $order->order_id;
+            }
+        }
+
         if (!empty($order->expected_pickup_time) && ($order->type != 11)) {
             // $params['expected_delivery_time'] = $order->expected_delivery_time;
             $params['expected_pickup_time'] = $order->expected_pickup_time;
