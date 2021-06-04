f390573f85b9397deae3206e60ca81084461af79
zhangzhen <zhangzhen@yeah.net>
2021-06-05 00:16:06 +0800

    对接达达

--- app/Http/Controllers/Api/Waimai/EleOrderController.php
+++ app/Http/Controllers/Api/Waimai/EleOrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Waimai;
 
 use App\Http\Controllers\Controller;
+use App\Libraries\Ele\Api\Tool;
 use Illuminate\Http\Request;
 
 class EleOrderController extends Controller
@@ -10,11 +11,31 @@ class EleOrderController extends Controller
     public function order(Request $request)
     {
         \Log::info("[饿了么]-[订单回调]，全部参数", $request->all());
-    }
 
+        return $this->res('resp.order.create');
+    }
 
     public function auth(Request $request)
     {
         \Log::info("[饿了么]-[授权回调]，全部参数", $request->all());
+    }
+
+    public function res($cmd)
+    {
+        $data = [
+            'body' => json_encode([
+                'errno' => 0,
+                'error' => 'success'
+            ]),
+            'cmd' => $cmd,
+            'source' => config("ps.ele.app_key"),
+            'ticket' => Tool::ticket(),
+            'timestamp' => time(),
+            'version' => 3
+        ];
+
+        $data['sign'] = Tool::getSign($data, config("ele.secret"));
+
+        return json_encode($data);
     }
 }
