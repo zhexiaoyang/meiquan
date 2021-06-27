f079f370d864176cbd38d9f2efaa1287d8794d2e
zhangzhen <zhangzhen@yeah.net>
2021-06-27 08:51:36 +0800

    对接达达

--- app/Http/Controllers/UserMoneyBalanceController.php
+++ app/Http/Controllers/UserMoneyBalanceController.php
@@ -10,9 +10,16 @@ class UserMoneyBalanceController extends Controller
     public function index(Request $request)
     {
         $page_size = $request->get("page_size", 10);
+        $type = $request->get("type", 0);
         $user = $request->user();
 
-        $data = UserMoneyBalance::where("user_id", $user->id)->orderBy("id", "desc")->paginate($page_size);
+        $query = UserMoneyBalance::where("user_id", $user->id);
+
+        if (in_array($type, [1, 2])) {
+            $query->where("type", $type);
+        }
+
+        $data = $query->orderBy("id", "desc")->paginate($page_size);
 
         return $this->page($data);
     }
