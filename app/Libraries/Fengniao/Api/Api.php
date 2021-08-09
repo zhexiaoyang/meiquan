e737cc490c066da086448baf7c3397fe6a4a7d4b
zhangzhen <zhangzhen@yeah.net>
2021-08-09 18:01:46 +0800

    对接达达

--- app/Libraries/Fengniao/Api/Api.php
+++ app/Libraries/Fengniao/Api/Api.php
@@ -124,7 +124,7 @@ class Api extends Request
             // "require_payment_pay" => 50.00,
             // 商品数量
             "goods_count" => 4,
-            "require_receive_time" => $order->expected_delivery_time * 1000,
+            // "require_receive_time" => $order->expected_delivery_time * 1000,
             // "serial_number" => $order->goods_pickup_info,
             "receiver_info" => [
                 "receiver_name" => $order->receiver_name,
@@ -227,5 +227,156 @@ class Api extends Request
     public function route($data)
     {
         return $this->post('v2/order/carrier_route', $data);
+    }
+
+    /**
+     * *****************************************************************************************************************
+     * *****************************************************************************************************************
+     * *************************************************  新 接 口 分 割 线  *********************************************
+     * *****************************************************************************************************************
+     * *****************************************************************************************************************
+     */
+
+    /**
+     * 预下单
+     * @param Shop $shop
+     * @param Order $order
+     * @return mixed
+     * @author zhangzhen
+     * @data 2021/6/12 2:09 下午
+     */
+    public function preCreateOrderNew(Shop $shop, Order $order)
+    {
+        $data = [
+            'partner_order_code' => (string) $order->order_id,
+            // 'out_shop_code' => (string) $shop->id,
+            'out_shop_code' => 'testorder001',
+            'order_type' => 1,
+            'position_source' => 3,
+            'receiver_address' => $order->receiver_address,
+            'receiver_longitude' => (double) $order->receiver_lng,
+            'receiver_latitude' => (double) $order->receiver_lat,
+            'goods_total_amount_cent' => 11.22,
+            'goods_actual_amount_cent' => (double) $order->goods_value,
+            'goods_weight' => 2.1,
+            'goods_count' => 1,
+            'goods_item_list' => [
+                [
+                    'item_name' => '商品',
+                    'item_quantity' => 2,
+                    'item_amount_cent' => 10.1,
+                    'item_actual_amount_cent' => 10.1,
+                ]
+            ]
+        ];
+
+        \Log::info($data);
+
+        return $this->postV3('v3/invoke/preCreateOrder', $data);
+    }
+
+    public function createOrderNew(Shop $shop, Order $order)
+    {
+        $data = [
+            'partner_order_code' => (string) $order->order_id,
+            // 'out_shop_code' => (string) $shop->id,
+            'out_shop_code' => 'testorder001',
+            'order_type' => 1,
+            'position_source' => 3,
+            'receiver_name' => $order->receiver_name,
+            'receiver_primary_phone' => $order->receiver_phone,
+            'receiver_address' => $order->receiver_address,
+            // 'receiver_longitude' => (double) $order->receiver_lng,
+            'receiver_longitude' => 116.307892,
+            // 'receiver_latitude' => (double) $order->receiver_lat,
+            'receiver_latitude' => 40.039115,
+            'goods_total_amount_cent' => 11.22,
+            'goods_actual_amount_cent' => (double) $order->goods_value,
+            'goods_weight' => 2.1,
+            'goods_count' => 1,
+            'goods_item_list' => [
+                [
+                    'item_name' => '商品',
+                    'item_quantity' => 2,
+                    'item_amount_cent' => 10.1,
+                    'item_actual_amount_cent' => 10.1,
+                ]
+            ]
+        ];
+
+        \Log::info($data);
+
+        return $this->postV3('v3/invoke/createOrder', $data);
+    }
+
+    /**
+     * 查询门店信息
+     * @param $id
+     * @return mixed
+     * @author zhangzhen
+     * @data 2021/6/12 3:01 下午
+     */
+    public function chainstoreQuery($id)
+    {
+        $data = [
+            'out_shop_code' => $id
+        ];
+
+        return $this->postV3('v3/invoke/chainstoreQuery', $data);
+    }
+
+    /**
+     * 查询门店范围
+     * @param $id
+     * @return mixed
+     * @author zhangzhen
+     * @data 2021/6/12 3:02 下午
+     */
+    public function chainstoreRange($id)
+    {
+        $data = [
+            'out_shop_code' => $id
+        ];
+
+        return $this->postV3('v3/invoke/chainstoreRange', $data);
+    }
+
+    public function getOrderDetail($order_id)
+    {
+        $data = [
+            'partner_order_code' => $order_id
+        ];
+
+        return $this->postV3('v3/invoke/getOrderDetail', $data);
+    }
+
+    public function getCancelReasonList($order_id)
+    {
+        $data = [
+            'partner_order_code' => $order_id
+        ];
+
+        return $this->postV3('v3/invoke/getCancelReasonList', $data);
+    }
+
+    public function preCancelOrder($order_id)
+    {
+        $data = [
+            'partner_order_code' => $order_id,
+            'order_cancel_code' => 9
+        ];
+
+        return $this->postV3('v3/invoke/preCancelOrder', $data);
+    }
+
+    public function cancelOrderNew($order_id)
+    {
+        $data = [
+            'partner_order_code' => $order_id,
+            'order_cancel_code' => 9,
+            'order_cancel_role' => 2
+        ];
+
+        return $this->postV3('v3/invoke/cancelOrder', $data);
     }
 }
