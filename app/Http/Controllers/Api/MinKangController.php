778030530d7339ff9a6b4bead665ce21a7ca8ebf
zhangzhen <zhangzhen@yeah.net>
2021-08-12 13:27:58 +0800

    对接达达

--- app/Http/Controllers/Api/MinKangController.php
+++ app/Http/Controllers/Api/MinKangController.php
@@ -22,91 +22,91 @@ class MinKangController
             $mt_shop_id = $request->get("app_poi_code", "");
             $mt_order_id = $request->get("wm_order_id_view", "");
             Log::info("【民康-推送已确认订单】（{$mt_order_id}）：开始");
-            $order_data = [
-                "order_id" => $mt_order_id,
-                // "order_tag_list" => urldecode($request->get("order_tag_list", "")),
-                "wm_order_id_view" => $request->get("wm_order_id_view", ""),
-                "app_poi_code" => $mt_shop_id,
-                "wm_poi_name" => urldecode($request->get("wm_poi_name", "")),
-                "wm_poi_address" => urldecode($request->get("wm_poi_address", "")),
-                "wm_poi_phone" => $request->get("wm_poi_phone", ""),
-                "recipient_address" => urldecode($request->get("recipient_address", "")),
-                "recipient_phone" => $request->get("recipient_phone", ""),
-                // "backup_recipient_phone" => urldecode($request->get("backup_recipient_phone", "")),
-                "recipient_name" => urldecode($request->get("recipient_name", "")) ?? "无名客人",
-                "shipping_fee" => $request->get("shipping_fee", 0),
-                "total" => $request->get("total", 0),
-                "original_price" => $request->get("original_price", 0),
-                "caution" => urldecode($request->get("caution", "")),
-                "shipper_phone" => $request->get("shipper_phone", "") ?? "",
-                "status" => $request->get("status", 0),
-                "ctime" => $request->get("ctime", 0),
-                "utime" => $request->get("utime", 0),
-                "delivery_time" => $request->get("delivery_time", 0),
-                "is_third_shipping" => $request->get("is_third_shipping", 0) ?? 0,
-                "pick_type" => $request->get("pick_type", 0) ?? 0,
-                "latitude" => $request->get("latitude", 0),
-                "longitude" => $request->get("longitude", 0),
-                "invoice_title" => $request->get("invoice_title", "") ?? "",
-                "day_seq" => $request->get("day_seq", 0) ?? 0,
-                "logistics_code" => $request->get("logistics_code", "") ?? "",
-                "package_bag_money" => $request->get("package_bag_money", 0) ?? 0,
-                "package_bag_money_yuan" => $request->get("package_bag_money_yuan", "") ?? 0,
-                "total_weight" => $request->get("total_weight", 0) ?? 0,
-                "mt_created_at" => date("Y-m-d H:i:s", $request->get("ctime", 0)),
-                "mt_updated_at" => date("Y-m-d H:i:s", $request->get("utime", 0)),
-            ];
-
-            // 商家对账
-            $poi_receive_detail_yuan = $request->get("poi_receive_detail_yuan", "") ?? "";
-            if ($poi_receive_detail_yuan) {
-                $order_data["poi_receive_detail_yuan"] = urldecode($poi_receive_detail_yuan);
-            }
-            // 订单优惠信息
-            // $extras = $request->get("extras");
-            // if ($extras) {
-            //     $order_data["extras"] = urldecode($extras);
+            // $order_data = [
+            //     "order_id" => $mt_order_id,
+            //     // "order_tag_list" => urldecode($request->get("order_tag_list", "")),
+            //     "wm_order_id_view" => $request->get("wm_order_id_view", ""),
+            //     "app_poi_code" => $mt_shop_id,
+            //     "wm_poi_name" => urldecode($request->get("wm_poi_name", "")),
+            //     "wm_poi_address" => urldecode($request->get("wm_poi_address", "")),
+            //     "wm_poi_phone" => $request->get("wm_poi_phone", ""),
+            //     "recipient_address" => urldecode($request->get("recipient_address", "")),
+            //     "recipient_phone" => $request->get("recipient_phone", ""),
+            //     // "backup_recipient_phone" => urldecode($request->get("backup_recipient_phone", "")),
+            //     "recipient_name" => urldecode($request->get("recipient_name", "")) ?? "无名客人",
+            //     "shipping_fee" => $request->get("shipping_fee", 0),
+            //     "total" => $request->get("total", 0),
+            //     "original_price" => $request->get("original_price", 0),
+            //     "caution" => urldecode($request->get("caution", "")),
+            //     "shipper_phone" => $request->get("shipper_phone", "") ?? "",
+            //     "status" => $request->get("status", 0),
+            //     "ctime" => $request->get("ctime", 0),
+            //     "utime" => $request->get("utime", 0),
+            //     "delivery_time" => $request->get("delivery_time", 0),
+            //     "is_third_shipping" => $request->get("is_third_shipping", 0) ?? 0,
+            //     "pick_type" => $request->get("pick_type", 0) ?? 0,
+            //     "latitude" => $request->get("latitude", 0),
+            //     "longitude" => $request->get("longitude", 0),
+            //     "invoice_title" => $request->get("invoice_title", "") ?? "",
+            //     "day_seq" => $request->get("day_seq", 0) ?? 0,
+            //     "logistics_code" => $request->get("logistics_code", "") ?? "",
+            //     "package_bag_money" => $request->get("package_bag_money", 0) ?? 0,
+            //     "package_bag_money_yuan" => $request->get("package_bag_money_yuan", "") ?? 0,
+            //     "total_weight" => $request->get("total_weight", 0) ?? 0,
+            //     "mt_created_at" => date("Y-m-d H:i:s", $request->get("ctime", 0)),
+            //     "mt_updated_at" => date("Y-m-d H:i:s", $request->get("utime", 0)),
+            // ];
+            //
+            // // 商家对账
+            // $poi_receive_detail_yuan = $request->get("poi_receive_detail_yuan", "") ?? "";
+            // if ($poi_receive_detail_yuan) {
+            //     $order_data["poi_receive_detail_yuan"] = urldecode($poi_receive_detail_yuan);
             // }
-            // 商品优惠信息
-            // $sku_benefit_detail = $request->get("sku_benefit_detail");
-            // if ($sku_benefit_detail) {
-            //     $order_data["sku_benefit_detail"] = urldecode($sku_benefit_detail);
+            // // 订单优惠信息
+            // // $extras = $request->get("extras");
+            // // if ($extras) {
+            // //     $order_data["extras"] = urldecode($extras);
+            // // }
+            // // 商品优惠信息
+            // // $sku_benefit_detail = $request->get("sku_benefit_detail");
+            // // if ($sku_benefit_detail) {
+            // //     $order_data["sku_benefit_detail"] = urldecode($sku_benefit_detail);
+            // // }
+            // // 订单商品信息
+            // $detail = $request->get("detail");
+            //
+            // // 创建订单
+            // $order = new MkOrder($order_data);
+            // if ($order->save() && $detail) {
+            //     Log::info("【民康-推送已确认订单】（{$mt_order_id}）：订单信息创建完毕");
+            //     $products = json_decode(urldecode($detail), true);
+            //     if (!empty($products)) {
+            //         // Log::info('民康-已确认订单-商品列表', $products);
+            //         $goods_price = 0;
+            //         $items = [];
+            //         foreach ($products as $product) {
+            //             $goods_price += $product['price'] * 100 * $product['quantity'];
+            //             $tmp['order_id'] = $order->id;
+            //             $tmp['mt_order_id'] = $mt_order_id;
+            //             $tmp['app_food_code'] = $product['app_food_code'];
+            //             $tmp['food_name'] = $product['food_name'];
+            //             $tmp['sku_id'] = $product['sku_id'];
+            //             $tmp['upc'] = $product['upc'];
+            //             $tmp['quantity'] = $product['quantity'];
+            //             $tmp['price'] = $product['price'];
+            //             $tmp['box_num'] = $product['box_num'];
+            //             $tmp['box_price'] = $product['box_price'];
+            //             $tmp['unit'] = $product['unit'];
+            //             $tmp['spec'] = $product['spec'] ?? "";
+            //             $tmp['weight'] = $product['weight'];
+            //             $items[] = $tmp;
+            //         }
+            //     }
+            //     $order->goods_price = $goods_price / 100;
+            //     $order->save();
+            //     MkOrderItem::query()->insert($items);
+            //     Log::info("【民康-推送已确认订单】（{$mt_order_id}）：商品信息创建完毕");
             // }
-            // 订单商品信息
-            $detail = $request->get("detail");
-
-            // 创建订单
-            $order = new MkOrder($order_data);
-            if ($order->save() && $detail) {
-                Log::info("【民康-推送已确认订单】（{$mt_order_id}）：订单信息创建完毕");
-                $products = json_decode(urldecode($detail), true);
-                if (!empty($products)) {
-                    // Log::info('民康-已确认订单-商品列表', $products);
-                    $goods_price = 0;
-                    $items = [];
-                    foreach ($products as $product) {
-                        $goods_price += $product['price'] * 100 * $product['quantity'];
-                        $tmp['order_id'] = $order->id;
-                        $tmp['mt_order_id'] = $mt_order_id;
-                        $tmp['app_food_code'] = $product['app_food_code'];
-                        $tmp['food_name'] = $product['food_name'];
-                        $tmp['sku_id'] = $product['sku_id'];
-                        $tmp['upc'] = $product['upc'];
-                        $tmp['quantity'] = $product['quantity'];
-                        $tmp['price'] = $product['price'];
-                        $tmp['box_num'] = $product['box_num'];
-                        $tmp['box_price'] = $product['box_price'];
-                        $tmp['unit'] = $product['unit'];
-                        $tmp['spec'] = $product['spec'] ?? "";
-                        $tmp['weight'] = $product['weight'];
-                        $items[] = $tmp;
-                    }
-                }
-                $order->goods_price = $goods_price / 100;
-                $order->save();
-                MkOrderItem::query()->insert($items);
-                Log::info("【民康-推送已确认订单】（{$mt_order_id}）：商品信息创建完毕");
-            }
 
             // 创建跑腿订单
             if ($shop = Shop::query()->where("mt_shop_id", $mt_shop_id)->first()) {
