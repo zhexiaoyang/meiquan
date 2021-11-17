458585174914d5bb15bf953d8cbad4da69ad0b0a
zhangzhen <zhangzhen@yeah.net>
2021-11-17 15:18:06 +0800

    打印机修改

--- app/Models/WmOrder.php
+++ app/Models/WmOrder.php
@@ -8,7 +8,7 @@ class WmOrder extends Model
 {
     //
     protected $fillable = ["shop_id","order_id","wm_order_id_view","platform","from_type","app_poi_code","wm_shop_name",
-        "recipient_name","recipient_phone","recipient_address","latitude","longitude","shipping_fee","total",
+        "recipient_name","recipient_phone","recipient_address","latitude","longitude","shipping_fee","total","logistics_code",
         "original_price","caution","shipper_phone","status","ctime","utime","delivery_time","pick_type","day_seq"];
 
     public function items()
