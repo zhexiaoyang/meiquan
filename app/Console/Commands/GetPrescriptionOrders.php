7de6d82bd542ef4544b6ce109184765c360a680d
zhangzhen <zhangzhen@yeah.net>
2022-01-29 21:34:50 +0800

    美全达

--- app/Console/Commands/GetPrescriptionOrders.php
+++ app/Console/Commands/GetPrescriptionOrders.php
@@ -47,7 +47,7 @@ class GetPrescriptionOrders extends Command
      */
     public function handle()
     {
-        $start_date = '2022-01-27';
+        $start_date = '2022-01-28';
         // $last = WmPrescription::where('platform', 1)->orderByDesc('id')->first();
         // if ($last) {
         //     $last_date = substr($last->rpCreateTime, 0, 10);
