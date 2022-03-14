209dc528487beecfb963de55dce821e24934645e
zhangzhen <zhangzhen@yeah.net>
2022-03-14 09:44:23 +0800

    美全达

--- app/Exports/Admin/VipProductExport.php
+++ app/Exports/Admin/VipProductExport.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Contracts\Support\Responsable;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
-use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
 
-class VipProductExport implements WithStrictNullComparison, Responsable, FromQuery, WithMapping, WithHeadings, WithTitle, ShouldAutoSize
+class VipProductExport extends DefaultValueBinder implements WithStrictNullComparison, Responsable, FromQuery, WithMapping, WithHeadings, ShouldAutoSize, WithCustomValueBinder
 {
     use Exportable;
 
@@ -40,7 +43,7 @@ class VipProductExport implements WithStrictNullComparison, Responsable, FromQue
             $product->name,
             $product->spec,
             $product->medicine_no,
-            "'".$product->upc,
+            $product->upc,
             $product->price,
             $product->cost,
         ];
@@ -60,8 +63,13 @@ class VipProductExport implements WithStrictNullComparison, Responsable, FromQue
         ];
     }
 
-    public function title(): string
+    public function bindValue(Cell $cell, $value)
     {
-        return '商品信息表';
+        $column = $cell->getColumn();
+        if (in_array( $column, ['B','H'])) {
+            $cell->setValueExplicit($value, DataType::TYPE_STRING);
+            return true;
+        }
+        return parent::bindValue($cell, $value);
     }
 }
