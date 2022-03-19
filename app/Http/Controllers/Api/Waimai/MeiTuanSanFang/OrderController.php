e5c8af1238d0387c79f7b3d6b87900b49aab7a48
zhangzhen <zhangzhen@yeah.net>
2022-03-19 11:57:53 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanSanFang/OrderController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanSanFang/OrderController.php
@@ -35,9 +35,9 @@ class OrderController extends Controller
     {
         $this->prefix .= '-[取消]';
 
-        if ($order_id = $request->get("orderId", "")) {
+        // if ($order_id = $request->get("orderId", "")) {
             $this->log('全部参数', $request->all());
-        }
+        // }
 
         return json_encode(['data' => 'ok']);
     }
