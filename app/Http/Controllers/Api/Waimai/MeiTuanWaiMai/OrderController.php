11bdddee15412590760ff8765a148590e6e5ef42
zhangzhen <zhangzhen@yeah.net>
2022-04-14 15:07:51 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/OrderController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/OrderController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Api\Waimai\MeiTuanWaiMai;
 use App\Jobs\VipOrderSettlement;
 use App\Models\Order;
 use App\Models\OrderLog;
+use App\Models\VipProduct;
 use App\Models\WmOrder;
+use App\Models\WmOrderItem;
 use App\Traits\LogTool;
+use App\Traits\NoticeTool;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController
 {
-    use LogTool;
+    use LogTool, NoticeTool;
 
     public $prefix_title = '[美团外卖回调&###]';
 
@@ -28,8 +32,18 @@ class OrderController
     public function refund(Request $request, $platform)
     {
         if ($order_id = $request->get("order_id", "")) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&全部退款|订单号:{$order_id}", $this->prefix_title);
-            $this->log_info('美团外卖统一接口');
+            $notify_type = $request->get('notify_type');
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&全部退款|订单号:{$order_id},类型:{$notify_type}", $this->prefix_title);
+            if ($notify_type == 'agree') {
+                if ($order = WmOrder::where('order_id', $order_id)->first()) {
+                    WmOrder::where('id', $order->id)->update([
+                        'refund_status' => 1,
+                        'refund_fee' => $order->total,
+                        'refund_at' => date("Y-m-d H:i:s")
+                    ]);
+                }
+                $this->log_info('全部参数', $request->all());
+            }
         }
 
         return json_encode(['data' => 'ok']);
@@ -38,18 +52,90 @@ class OrderController
     public function partrefund(Request $request, $platform)
     {
         if ($order_id = $request->get("order_id", "")) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&部分退款|订单号:{$order_id}", $this->prefix_title);
-            $this->log_info('美团外卖统一接口');
+            $money = $request->get('money');
+            $notify_type = $request->get('notify_type');
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&部分退款|订单号:{$order_id},类型:{$notify_type},金额:{$money}", $this->prefix_title);
+            if (($notify_type == 'agree') && ($money > 0)) {
+                if ($order = WmOrder::where('order_id', $order_id)->first()) {
+                    $this->log_info('全部参数', $request->all());
+                    WmOrder::where('id', $order->id)->update([
+                        'refund_status' => 2,
+                        'refund_fee' => $money,
+                        'refund_at' => date("Y-m-d H:i:s"),
+                    ]);
+                    $food_str = $request->get('food');
+                    $foods = json_decode($food_str, true);
+                    if (!empty($foods)) {
+                        DB::transaction(function () use ($order, $foods) {
+                            $vip = $order->is_vip;
+                            $dec_cost = 0;
+                            $where['order_id'] = $order->id;
+                            foreach ($foods as $food) {
+                                $where['upc'] = $food['upc'];
+                                $count = $food['count'];
+                                if ($item = WmOrderItem::where($where)->where('quantity', '>', 0)->first()) {
+                                    WmOrderItem::where('id', $item->id)->update([
+                                        'quantity' => $item->quantity - $count,
+                                        'refund_quantity' => $count,
+                                    ]);
+                                }
+                                if ($vip) {
+                                    $cost = VipProduct::select('cost')->where(['upc' => $food['upc'], 'shop_id' => $order->shop_id])->first();
+                                    if (isset($cost->cost)) {
+                                        $dec_cost += ($cost->cost ?? 0);
+                                    } else {
+                                        $this->log_info("成本价不存在，订单ID:{$order->order_id}");
+                                    }
+                                }
+                            }
+                            $vip_cost = $order->vip_cost - $dec_cost;
+                            WmOrder::where('id', $order->id)->update(['vip_cost' => $vip_cost > 0 ? $vip_cost : 0]);
+                        });
+                        if ($order->is_vip) {
+                            // 如果是VIP订单，触发结算JOB
+                            dispatch(new VipOrderSettlement($order));
+                        }
+                    }
+                }
+            }
         }
-
         return json_encode(['data' => 'ok']);
     }
 
     public function rider(Request $request, $platform)
     {
-        if ($order_id = $request->get("order_id", "")) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&美配订单状态回调|订单号:{$order_id}", $this->prefix_title);
-            $this->log_info('美团外卖统一接口');
+        $order_id = $request->get('order_id', '');
+        $status = $request->get('logistics_status', '');
+        $time = $request->get('time', 0);
+        $name = $request->get('dispatcher_name', '');
+        $phone = $request->get('dispatcher_mobile', '');
+
+        if ($order_id && $status) {
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&美配订单状态回调|配送状态:{$status}|订单号:{$order_id}", $this->prefix_title);
+            if ($order = WmOrder::where('order_id', $order_id)->first()) {
+                if (in_array($status, [10, 20, 40]) && $order->status < 16) {
+                    if ($status == 10) {
+                        $order->status = 12;
+                        $order->receive_at = date("Y-m-d H:i:s", $time ?: time());
+                    } elseif ($status == 20) {
+                        $order->status = 14;
+                        $order->send_at = date("Y-m-d H:i:s", $time ?: time());
+                    } elseif ($status == 40) {
+                        $order->status = 16;
+                        $order->deliver_at = date("Y-m-d H:i:s", $time ?: time());
+                    }
+                    if ($name) {
+                        $order->shipper_name = $name;
+                        $order->shipper_phone = $phone;
+                    }
+                    $order->save();
+                    $this->log_info("订单号：{$order_id}|操作完成");
+                } else {
+                    $this->log_info("订单号：{$order_id}|操作失败|美团状态：{$status}|系统订单状态：{$order->status}");
+                }
+            } else {
+                $this->log_info("订单号：{$order_id}|订单不存在");
+            }
         }
 
         return json_encode(['data' => 'ok']);
@@ -57,11 +143,25 @@ class OrderController
 
     public function status_self(Request $request, $platform)
     {
-        if ($order_id = $request->get("order_id", "")) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&自配订单状态|订单号:{$order_id}", $this->prefix_title);
-            $this->log_info('美团外卖统一接口');
-        }
+        $order_id = $request->get('order_view_id', '');
+        $status = $request->get('logistics_status', '');
+        $time = $request->get('operate_time', 0);
 
+        if ($order_id && $status) {
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&自配订单状态|配送状态:{$status}|订单号:{$order_id}", $this->prefix_title);
+            if ($order = WmOrder::where('order_id', $order_id)->first()) {
+                if ($status == 20 && $order->status < 14) {
+                    $order->send_at = date("Y-m-d H:i:s", $time ?: time());
+                    $order->status = 14;
+                    $order->save();
+                    $this->log_info("订单号：{$order_id}|操作完成");
+                } else {
+                    $this->log_info("订单号：{$order_id}|操作失败|美团状态：{$status}|系统订单状态：{$order->status}");
+                }
+            } else {
+                $this->log_info("订单号：{$order_id}|订单不存在");
+            }
+        }
         return json_encode(['data' => 'ok']);
     }
 
@@ -75,7 +175,7 @@ class OrderController
         $status = $request->get('status', '');
 
         if ($order_id && $status) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&完成订单|订单号:{$order_id}", $this->prefix_title);
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&完成订单|订单状态:{$status}|订单号:{$order_id}", $this->prefix_title);
             if ($order = WmOrder::where('order_id', $order_id)->first()) {
                 if ($status == 8 && $order->status < 18) {
                     $order->status = 18;
@@ -109,11 +209,20 @@ class OrderController
 
     public function settlement(Request $request, $platform)
     {
-        if ($order_id = $request->get("order_id", "")) {
-            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&订单结算|订单号:{$order_id}", $this->prefix_title);
-            $this->log_info('美团外卖统一接口');
+        $order_id = $request->get('order_id', '');
+        $status = $request->get('status', '');
+        $fee = $request->get('settleAmount', '');
+        if ($order_id && $status) {
+            $this->prefix = str_replace('###', get_meituan_develop_platform($platform) . "&订单结算|订单状态:{$status}|订单号:{$order_id}", $this->prefix_title);
+            if ($order = WmOrder::where('order_id', $order_id)->first()) {
+                $this->log_info('VIP全部参数', $request->all());
+                if (floatval($order->poi_receive) != floatval($fee)) {
+                    $this->ding_error("结算金额不一致异常,status:{$status},order_id:{$order_id},poi_receive:{$order->poi_receive},fee:{$fee},时间:".date("Y-m-d H:i:s"));
+                }
+            } else {
+                $this->log_info('全部参数', $request->all());
+            }
         }
-
         return json_encode(['data' => 'ok']);
     }
 
