0f837dced6db1ed8c047c733a3c466d32bf0dbde
zhangzhen <zhangzhen@yeah.net>
2022-04-29 12:33:41 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/ProductStockController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/ProductStockController.php
@@ -18,9 +18,11 @@ class ProductStockController
             return json_encode(["data" => "ok"]);
         }
         $wanxiang = ['12606969','12965411','12606971','12966872','13084144','13144836','12931358','12931400','13778180','12931402','13505397'];
+        $xuesong = ['9493159','9493161','9493163','9493216','9493164','9492506','9493165','9493089','9493167','9492507',
+            '9492509','9493168','9493172','9492664','9492666','9492670','9492671'];
         $wanxiang_mqp = ['12606969' => '0009','12965411' => '0004','12606971' => '0015','12966872' => '0012', '13084144' => '0003',
             '13144836' => '0006','12931358' => '0007','12931400' => '0007','13778180' => '0007','12931402' => '0007','13505397' => '0007'];
-        if (!in_array($shop_id, array_merge($wanxiang))) {
+        if (!in_array($shop_id, array_merge($wanxiang, $xuesong))) {
             return json_encode(["code" => 1, "message" => ""]);
         }
         $product_ids = [];
@@ -62,6 +64,29 @@ class ProductStockController
                 }
                 if (!empty($res_data)) {
                     $this->log_info("万祥商品库存拉取,万祥门店ID:{$hd_shop_id},返回值", $res_data);
+                    return json_encode(["code" => 0, "message" => "成功", "data" => $res_data]);
+                }
+            }
+        }
+
+        if (in_array($shop_id, $xuesong)) {
+            $xs_shop_id = $shop_id;
+            $this->log_info("雪松商品库存拉取,雪松门店ID:{$xs_shop_id}", $product_ids);
+            $product_in = implode(',', $product_ids);
+            // $data = DB::connection('xuesong')
+            //     ->select("SELECT 药品ID as id,库存 as stock FROM [dbo].[v_store_m_mtxs] WHERE [门店ID] = N'{$hd_shop_id}' AND [药品ID] IN ({$product_in})");
+            $data = DB::connection('xuesong')
+                ->select("SELECT bianhao as id, kucun as stock FROM [dbo].[v_meituan_kucun] WHERE [meituan] = N'{$xs_shop_id}' AND [bianhao] IN ({$product_in})");
+            if (!empty($data)) {
+                $res_data = [];
+                foreach ($data as $v) {
+                    $res_data[] = [
+                        'app_medicine_code' => $v->id,
+                        'stock' => intval($v->stock),
+                    ];
+                }
+                if (!empty($res_data)) {
+                    $this->log_info("雪松商品库存拉取,雪松门店ID:{$xs_shop_id},返回值", $res_data);
                     return json_encode(["code" => 0, "message" => "成功", "data" => $res_data]);
                 }
             }
