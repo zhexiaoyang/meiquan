3a71a1d75d88aeebc421d4065ed317f7e251680d
zhangzhen <zhangzhen@yeah.net>
2022-05-24 15:30:44 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanMeiquanController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanMeiquanController.php
@@ -67,7 +67,7 @@ class MeiTuanMeiquanController extends Controller
 
             // 处理门店
             // 查询门店个数
-            $shops = Shop::query()->where("waimai_mt", $shop_id)->get();
+            $shops = Shop::query()->where("mtwm", $shop_id)->get();
             if ($shop = $shops->first()) {
                 if ($shops->count() > 1) {
                     $this->ding_error("美团外卖ID，数量大于1");
