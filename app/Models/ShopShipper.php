5ebcaaf155ae2735b5023ca4ce93e645e4fa2378
zhangzhen <zhangzhen@yeah.net>
2022-05-27 11:35:15 +0800

    美全达

--- app/Models/ShopShipper.php
+++ app/Models/ShopShipper.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopShipper extends Model
 {
-    protected $fillable = ['user_id','shop_id','platform','three_id','access_token','refresh_token','expires_in','token_time'];
+    protected $fillable = ['user_id','shop_id','platform','three_id','source_id',
+        'access_token','refresh_token','expires_in','token_time'];
 }
