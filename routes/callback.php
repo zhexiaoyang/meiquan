38d62528831bc3fe3c062cfe1db0ea72b789f8a8
zhangzhen <zhangzhen@yeah.net>
2022-05-27 14:02:48 +0800

    美全达

--- routes/callback.php
+++ routes/callback.php
@@ -40,7 +40,7 @@ Route::middleware(['force-json'])->prefix('shansong')->namespace('Api\Callback')
 // 达达-服务商
 Route::middleware(['force-json'])->prefix('dada')->namespace('Api\Callback')->group(function () {
     // 达达订单回调
-    Route::post('order', "DaDaAuthController@order");
+    Route::post('order', "DaDaOrderController@order");
     // 达达门店绑定回调
     Route::get('auth', "DaDaAuthController@auth");
 });
