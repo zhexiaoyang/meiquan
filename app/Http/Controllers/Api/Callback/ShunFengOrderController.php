b7ce556955d1ff52cae02e68654b98b0c58dce09
zhangzhen <zhangzhen@yeah.net>
2022-06-16 14:57:02 +0800

    美全达

--- app/Http/Controllers/Api/Callback/ShunFengOrderController.php
+++ app/Http/Controllers/Api/Callback/ShunFengOrderController.php
@@ -454,4 +454,11 @@ class ShunFengOrderController extends Controller
 
         return json_encode($res);
     }
+
+    public function revoke(Request $request)
+    {
+        \Log::info("顺丰revoke", $request->all());
+        $res = ["error_code" => 0, "error_msg" => "success"];
+        return json_encode($res);
+    }
 }
