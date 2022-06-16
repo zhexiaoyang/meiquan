71fab78c2f3694a6ae5c8ac62be9b38ee9ab571e
zhangzhen <zhangzhen@yeah.net>
2022-06-16 12:17:17 +0800

    美全达

--- app/Task/TakeoutOrderVoiceNoticeTask.php
+++ app/Task/TakeoutOrderVoiceNoticeTask.php
@@ -11,9 +11,10 @@ class TakeoutOrderVoiceNoticeTask extends Task
     public $user_id;
 
     // 1 新订单，2 预订单，5 催单，7 退款，9 取消订单
-    // 11 骑手接单，14 订单送达，17 配送失败提示
-    // 21 超时10分钟未接单，23 超时20分钟未接单， 27 取货30分钟未送达
-    // 31 跑腿账户余额不足提示
+    // 11 骑手接单，14 订单送达，17 配送失败提示(未)
+    // 21 超时10分钟未接单（未），23 超时20分钟未接单（未）， 27 取货30分钟未送达（未）
+    // 31 跑腿账户余额不足提示（未）
+    //
     public function __construct($voice, $user_id)
     {
         $this->voice = $voice;
@@ -37,6 +38,20 @@ class TakeoutOrderVoiceNoticeTask extends Task
             }
         }
         if ($fd_str = Redis::hget('h:websocket:note_voice:user', 1)) {
+            $res = [
+                'mes' => 'success',
+                'kind' => 'voice',
+                'voice' => $this->voice
+            ];
+            $res = json_encode($res, true);
+            $fds = explode(',', $fd_str);
+            $server = app('swoole');
+            foreach ($fds as $fd) {
+                \Log::info("fd:{$fd},res:{$res}");
+                $server->push($fd, $res);
+            }
+        }
+        if ($fd_str = Redis::hget('h:websocket:note_voice:user', 32)) {
             $res = [
                 'mes' => 'success',
                 'kind' => 'voice',
