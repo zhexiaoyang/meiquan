d71d7de5146b32a3458918de9da95e40fad3ba94
zhangzhen <zhangzhen@yeah.net>
2022-07-02 21:09:26 +0800

    美全达

--- app/Console/Commands/SyncCodeBeiKang.php
+++ app/Console/Commands/SyncCodeBeiKang.php
@@ -41,35 +41,35 @@ class SyncCodeBeiKang extends Command
     {
         $minkang = app("minkang");
         // --------------------- 桐君阁大药房（北城天街店）:5910555 ---------------------
-        // $this->info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-开始......');
-        // Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-开始......');
-        // // $data = DB::connection('wanxiang_haidian')
-        // //     ->select("SELECT 药品ID as id,upc,库存 as stock FROM [dbo].[v_store_m_mtxs] WHERE [门店ID] = N'0017' AND [upc] <> '' AND [upc] IS NOT NULL");
-        // $data = DB::connection('beikang')
-        //     ->select("SELECT 门店商品编码 as id, 药品条形码 as upc FROM [dbo].[商品库存清单] WHERE [门店编码] = N'38店' AND [药品条形码] <> '' AND [药品条形码] IS NOT NULL");
-        // if (!empty($data)) {
-        //     // $this->info('门店「桐君阁大药房（北城天街店）:5910555」不是空数据......');
-        //     $data = array_chunk($data, 200);
-        //     foreach ($data as $items) {
-        //         $code_data = [];
-        //         foreach ($items as $item) {
-        //             $code_data[] = [
-        //                 'upc' => trim($item->upc),
-        //                 'app_medicine_code_new' => trim($item->id),
-        //             ];
-        //         }
-        //
-        //         // 绑定商品编码
-        //         $params['app_poi_code'] = '5910555';
-        //         $params['medicine_data'] = json_encode($code_data);
-        //         $res = $minkang->medicineCodeUpdate($params);
-        //         // Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......', [$res]);
-        //     }
-        // } else {
-        //     $this->info('门店「桐君阁大药房（北城天街店）:5910555」空数据......');
-        // }
-        // $this->info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......');
-        // Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......');
+        $this->info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-开始......');
+        Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-开始......');
+        // $data = DB::connection('wanxiang_haidian')
+        //     ->select("SELECT 药品ID as id,upc,库存 as stock FROM [dbo].[v_store_m_mtxs] WHERE [门店ID] = N'0017' AND [upc] <> '' AND [upc] IS NOT NULL");
+        $data = DB::connection('beikang')
+            ->select("SELECT 门店商品编码 as id, 药品条形码 as upc FROM [dbo].[商品库存清单] WHERE [门店编码] = N'38店' AND [药品条形码] <> '' AND [药品条形码] IS NOT NULL");
+        if (!empty($data)) {
+            // $this->info('门店「桐君阁大药房（北城天街店）:5910555」不是空数据......');
+            $data = array_chunk($data, 200);
+            foreach ($data as $items) {
+                $code_data = [];
+                foreach ($items as $item) {
+                    $code_data[] = [
+                        'upc' => trim($item->upc),
+                        'app_medicine_code_new' => trim($item->id),
+                    ];
+                }
+
+                // 绑定商品编码
+                $params['app_poi_code'] = '5910555';
+                $params['medicine_data'] = json_encode($code_data);
+                $res = $minkang->medicineCodeUpdate($params);
+                // Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......', [$res]);
+            }
+        } else {
+            $this->info('门店「桐君阁大药房（北城天街店）:5910555」空数据......');
+        }
+        $this->info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......');
+        Log::info('门店「桐君阁大药房（北城天街店）:5910555」编码绑定同步-结束......');
 
         $shops = [
             '桐君阁大药房(湖云街店)' => '5910549',
