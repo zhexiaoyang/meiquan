4ecf97265d96153e6e9b6f8a513fd6c6e315a3ad
zhangzhen <zhangzhen@yeah.net>
2022-08-24 20:04:07 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/ProductController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanWaiMai/ProductController.php
@@ -110,7 +110,6 @@ class ProductController
                         if ($product = VipProduct::query()->where('shop_id', $shop->id)->where('upc', $upc)->first()) {
                             if (VipProduct::query()->where('id', $product->id)->update(['price' => $price])) {
                                 $this->log_info("更新VIP商品成功|门店:{$shop->id},门店:{$app_poi_code},upc:{$upc}");
-                                $this->ding_exception("更新VIP商品成功|门店:{$shop->id},门店:{$app_poi_code},upc:{$upc}");
                             } else {
                                 // $this->ding_exception("更新VIP商品成功|门店:{$shop->id},门店:{$app_poi_code},upc:{$upc}");
                             }
