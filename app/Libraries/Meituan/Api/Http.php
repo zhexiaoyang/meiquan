258e2bc42117e07538f84555b83f69831f6f8e12
zhangzhen <zhangzhen@yeah.net>
2022-09-22 10:58:57 +0800

    美全达

--- app/Libraries/Meituan/Api/Http.php
+++ app/Libraries/Meituan/Api/Http.php
@@ -2,6 +2,7 @@
 
 namespace App\Libraries\Meituan\Api;
 
+use App\Libraries\DingTalk\DingTalkRobotNotice;
 use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Client as HttpClient;
 
@@ -22,18 +23,24 @@ class Http
 
     public function request($method, $url, $options = [])
     {
-        $method = strtoupper($method);
-//        $options = array_merge(self::$defaults, $options);
-//         Log::debug('美团配送请求参数:', compact('url', 'method', 'options'));
-//        $options['handler'] = $this->getHandler();
-        $response = $this->getClient()->request($method, $url, $options);
-        // Log::debug('美团配送响应参数:', [
-        //     'Status'  => $response->getStatusCode(),
-        //     // 'Reason'  => $response->getReasonPhrase(),
-        //     // 'Headers' => $response->getHeaders(),
-        //     'Body'    => strval($response->getBody()),
-        // ]);
-        return $response;
+
+        try {
+            $method = strtoupper($method);
+            // $options = array_merge(self::$defaults, $options);
+            // Log::debug('美团配送请求参数:', compact('url', 'method', 'options'));
+            // $options['handler'] = $this->getHandler();
+            $response = $this->getClient()->request($method, $url, $options);
+            // Log::debug('美团配送响应参数:', [
+            //     'Status'  => $response->getStatusCode(),
+            //     // 'Reason'  => $response->getReasonPhrase(),
+            //     // 'Headers' => $response->getHeaders(),
+            //     'Body'    => strval($response->getBody()),
+            // ]);
+            return $response;
+        } catch (\Exception $e) {
+            $ding = new DingTalkRobotNotice("c957a526bb78093f61c61ef0693cc82aae34e079f4de3321ef14c881611204c4");
+            $ding->sendTextMsg("美团请求接口异常|{$e->getMessage()}|{$method}|{$url}|" . json_encode($options, JSON_UNESCAPED_UNICODE));
+        }
     }
 
     public function getClient()
