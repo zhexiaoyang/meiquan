ad590a6d23a2ab30ac353a11d7dcecce97bd053c
zhangzhen <zhangzhen@yeah.net>
2022-10-20 13:10:32 +0800

    美全达

--- app/Console/Commands/SyncStockRuiZhiJia.php
+++ app/Console/Commands/SyncStockRuiZhiJia.php
@@ -72,7 +72,7 @@ class SyncStockRuiZhiJia extends Command
                     $stock_data = [];
                     foreach ($items as $item) {
                         $stock_data[] = [
-                            'app_medicine_code' => trim($item->id),
+                            'app_medicine_code' => trim($item->upc),
                             'stock' => (int) $item->stock,
                         ];
                     }
