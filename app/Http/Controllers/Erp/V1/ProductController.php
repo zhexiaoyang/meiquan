ea4605da8032b8d38a3229fc16acfa5e541b89a1
zhangzhen <zhangzhen@yeah.net>
2023-01-06 11:57:59 +0800

    美全达

--- app/Http/Controllers/Erp/V1/ProductController.php
+++ app/Http/Controllers/Erp/V1/ProductController.php
@@ -401,7 +401,8 @@ class ProductController extends Controller
                             'upc' => $item['upc'],
                             'price' => $item['price'],
                             'stock' => $item['stock'],
-                            'category_code' => isset($upc_pluck[$item['upc']]) ? $upc_pluck[$item['upc']] : 9000000,
+                            // 'category_code' => isset($upc_pluck[$item['upc']]) ? $upc_pluck[$item['upc']] : 9000000,
+                            'category_code' => 9000000,
                             'is_sold_out' => 0,
                             'sequence' => 100
                         ];
