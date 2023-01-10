3bf6d211bd163e82fa0b31c9a289ec57085d3ad0
zhangzhen <zhangzhen@yeah.net>
2023-01-10 20:28:00 +0800

    美全达

--- app/Console/Commands/SyncStockRuiZhiJia.php
+++ app/Console/Commands/SyncStockRuiZhiJia.php
@@ -65,7 +65,7 @@ class SyncStockRuiZhiJia extends Command
             $this->info("门店「{$name}}:{$id}」库存同步-开始......");
             Log::info("门店「{$name}}:{$id}」库存同步-开始......");
             $data = DB::connection('ruizhijia')
-                ->select("SELECT 商品条码 as id, 库存数量 as stock FROM [dbo].[vZtGoods] WHERE [门店名称] = N'{$name}' AND [商品条码] <> '' AND [商品条码] IS NOT NULL");
+                ->select("SELECT 商品条码 as id, 库存数量 as stock FROM [dbo].[vZtGoods] WHERE [门店名称] = N'{$name}' AND [商品条码] <> '' AND [商品条码] IS NOT NULL GROUP BY [商品条码]");
             if (!empty($data)) {
                 $data = array_chunk($data, 200);
                 foreach ($data as $items) {
