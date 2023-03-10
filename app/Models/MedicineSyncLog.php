0464b758728096a38efaebacb9c5edb915642afa
zhangzhen <zhangzhen@yeah.net>
2023-03-10 20:29:54 +0800

    美全达

--- app/Models/MedicineSyncLog.php
+++ app/Models/MedicineSyncLog.php
@@ -8,5 +8,5 @@ class MedicineSyncLog extends Model
 {
     protected $table = 'wm_medicine_sync_logs';
 
-    protected $fillable = ['shop_id', 'platform', 'total', 'success', 'fail', 'error', 'status', 'log_id'];
+    protected $fillable = ['shop_id', 'platform', 'total', 'success', 'fail', 'error', 'mt_success', 'mt_fail', 'ele_success', 'ele_fail', 'status', 'log_id'];
 }
