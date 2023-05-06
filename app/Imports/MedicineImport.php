bb651f4143b2dced27c9c5d8168e1e629cadc13c
zhangzhen <zhangzhen@yeah.net>
2023-05-06 23:44:36 +0800

    美全达

--- app/Imports/MedicineImport.php
+++ app/Imports/MedicineImport.php
@@ -23,7 +23,7 @@ class MedicineImport implements ToCollection, WithHeadingRow, WithValidation, Wi
         $row = $row->toArray();
         // 第一行是说明，去掉
         array_shift($row);
-        // throw new InvalidRequestException($row[0]['商品条码'], 422);
+        // throw new InvalidRequestException($row[0]['条形码'], 422);
         if (count($row) > 5000) {
             throw new InvalidRequestException('药品数量不能超过5000', 422);
         }
@@ -32,35 +32,61 @@ class MedicineImport implements ToCollection, WithHeadingRow, WithValidation, Wi
         }
 
         $chongfu = [];
+        $chongfu_store_id = [];
 
         foreach ($row as $k => $item) {
+            // 行数+3
             $line = $k + 3;
-            if (!isset($item['商品条码'])) {
-                throw new InvalidRequestException("第{$line}不存在商品条码", 422);
+            if (isset($chongfu_store_id[$item['商家商品编码']])) {
+                throw new InvalidRequestException("第{$line}行商家商品编码与第{$chongfu[$item['商家商品编码']]}行商家商品编码重复", 422);
             }
-            if (isset($chongfu[$item['商品条码']])) {
-                throw new InvalidRequestException("第{$line}行商品条码与第{$chongfu[$item['商品条码']]}行商品条码重复", 422);
+            if (!isset($item['条形码'])) {
+                throw new InvalidRequestException("第{$line}行不存在条形码", 422);
             }
-            if (empty($item['商品条码'])) {
-                throw new InvalidRequestException("第{$line}商品条码不能为空", 422);
+            // if (!empty(trim($item['商家商品编码']))) {
+            //     if ($m = Medicine::select('store_id', 'upc')->where('store_id', trim($item['商家商品编码']))->first()) {
+            //         if ($m->upc != trim($item['商家商品编码'])) {
+            //             throw new InvalidRequestException("第{$line}行商家商品编码已存在，绑定商品条码：" . $m->upc, 422);
+            //         }
+            //     }
+            // }
+            if (isset($chongfu[$item['条形码']])) {
+                throw new InvalidRequestException("第{$line}行条形码与第{$chongfu[$item['条形码']]}行条形码重复", 422);
             }
-            if (!isset($item['销售价'])) {
-                throw new InvalidRequestException("第{$line}不存在销售价", 422);
+            if (empty($item['条形码'])) {
+                throw new InvalidRequestException("第{$line}行条形码不能为空", 422);
             }
-            if (!is_numeric($item['销售价'])) {
-                throw new InvalidRequestException("第{$line}销售价，格式不正确", 422);
+            if (!isset($item['线上销售价格'])) {
+                throw new InvalidRequestException("第{$line}行不存在线上销售价格", 422);
             }
-            if (!isset($item['成本价'])) {
-                throw new InvalidRequestException("第{$line}不存在成本价", 422);
+            if (!is_numeric(trim($item['线上销售价格']))) {
+                throw new InvalidRequestException("第{$line}行线上销售价格，格式不正确", 422);
             }
-            if (!is_numeric($item['成本价'])) {
-                throw new InvalidRequestException("第{$line}成本价，格式不正确", 422);
+            if ($item['线上销售价格'] < 0) {
+                throw new InvalidRequestException("第{$line}行线上销售价格，不能小于0", 422);
+            }
+            if (!isset($item['成本价格'])) {
+                throw new InvalidRequestException("第{$line}行不存在成本价格", 422);
+            }
+            if (!is_numeric(trim($item['成本价格']))) {
+                throw new InvalidRequestException("第{$line}行成本价格，格式不正确", 422);
+            }
+            if ($item['成本价格'] < 0) {
+                throw new InvalidRequestException("第{$line}行成本价格，不能小于0", 422);
             }
             if (!isset($item['库存'])) {
-                throw new InvalidRequestException("第{$line}不存在库存", 422);
+                throw new InvalidRequestException("第{$line}行不存在库存", 422);
             }
-            $chongfu[$item['商品条码']] = $line;
+            if (!is_numeric(trim($item['库存']))) {
+                throw new InvalidRequestException("第{$line}行库存格式不正确", 422);
+            }
+            $chongfu[$item['条形码']] = $line;
+            // $chongfu[$item['商家商品编码']] = $line;
+            if (!empty(trim($item['商家商品编码']))) {
+                $chongfu[$item['商家商品编码']] = $line;
+            }
         }
+        // throw new InvalidRequestException("拦截拦截拦截", 422);
 
         // 添加日志
         $log = MedicineSyncLog::create([
@@ -74,14 +100,28 @@ class MedicineImport implements ToCollection, WithHeadingRow, WithValidation, Wi
             'error' => 0,
         ]);
         foreach ($row as $item) {
+            // 线下售价-非必填
+            $down_price = 0;
+            if (isset($item['线下销售价格']) && is_numeric(trim($item['线下销售价格']))) {
+                $down_price = trim($item['线下销售价格']);
+            }
+            // 排序-非必填
+            $sequence = 1000;
+            if (isset($item['排序']) && is_numeric(trim($item['排序'])) && trim($item['排序']) >= 0) {
+                $sequence = trim($item['排序']);
+            }
+
             $medicine_data = [
+                'store_id' => trim($item['商家商品编码']),
                 'name' => trim($item['商品名称']),
-                'upc' => trim($item['商品条码']),
+                'upc' => trim($item['条形码']),
                 'stock' => trim($item['库存']),
-                'price' => trim($item['销售价']),
-                'guidance_price' => trim($item['成本价']),
+                'price' => trim($item['线上销售价格']),
+                'down_price' => $down_price,
+                'guidance_price' => trim($item['成本价格']),
+                'sequence' => $sequence,
             ];
-            MedicineImportJob::dispatch($this->shop_id, $medicine_data, $log->id)->onQueue('medicine');
+            MedicineImportJob::dispatch($this->shop_id, $medicine_data, $log->id, $log->total)->onQueue('medicine');
         }
     }
 
@@ -99,7 +139,7 @@ class MedicineImport implements ToCollection, WithHeadingRow, WithValidation, Wi
     {
         return [
             '商品名称' => 'required|string',
-            '商品条码' => 'required|string',
+            '条形码' => 'required|string',
             '销售价' => 'required|numeric',
             '成本价' => 'required|numeric',
         ];
--- app/Jobs/MedicineImportJob.php
+++ app/Jobs/MedicineImportJob.php
@@ -23,6 +23,7 @@ class MedicineImportJob implements ShouldQueue
     public $medicine;
     public $shop_id;
     public $log_id;
+    public $log_total;
     // public $unique_key;
 
     /**
@@ -30,11 +31,12 @@ class MedicineImportJob implements ShouldQueue
      *
      * @return void
      */
-    public function __construct(int $shop_id, array $medicine, $log_id)
+    public function __construct(int $shop_id, array $medicine, $log_id, $log_total = 0)
     {
         $this->shop_id = $shop_id;
         $this->medicine = $medicine;
         $this->log_id = $log_id;
+        $this->log_total = $log_total;
         // $this->unique_key = uniqid();
         // \Log::info('进入JOB：' . $this->unique_key, [$log_id]);
     }
@@ -50,123 +52,147 @@ class MedicineImportJob implements ShouldQueue
         $price = $this->medicine['price'];
         $stock = $this->medicine['stock'];
         $cost = $this->medicine['guidance_price'];
+        $down_price = $this->medicine['down_price'] ?? 0;
+        $sequence = $this->medicine['sequence'] ?? 1000;
+        $store_id = $this->medicine['store_id'] ?? '';
         $status = true;
+        $msg = '药品添加成功';
+        $error_status = false;
 
-        if (!$medicine = Medicine::where('upc', $upc)->where('shop_id', $this->shop_id)->first()) {
-            if ($depot = MedicineDepot::where('upc', $upc)->first()) {
-                // 品库中有此商品
+        if ($store_id && $m = Medicine::where('store_id', $store_id)->where('shop_id', $this->shop_id)->first()) {
+            if ($upc != $m->upc) {
+                $error_status = true;
+                $msg = '该商家商品编码已存在，绑定商品条码：' . $m->upc;
+            }
+        }
 
-                // 创建分类
-                // 查找该商品在品库中的所有分类ID
-                $depot_category_ids = \DB::table('wm_depot_medicine_category')->where('medicine_id', $depot->id)->get()->pluck('category_id');
-                if (!empty($depot_category_ids)) {
-                    // 根据查找的分类ID，查找该商品所有分类
-                    $depot_categories = MedicineDepotCategory::whereIn('id', $depot_category_ids)->get();
-                    if (!empty($depot_categories)) {
-                        foreach ($depot_categories as $depot_category) {
-                            $pid = 0;
-                            if ($depot_category->pid != 0) {
-                                if ($depot_category_parent = MedicineDepotCategory::find($depot_category->pid)) {
-                                    try {
-                                        $category_parent = MedicineCategory::firstOrCreate(
-                                            [
+        if ($error_status) {
+            $status = false;
+        } else {
+            if (!Medicine::where('upc', $upc)->where('shop_id', $this->shop_id)->first()) {
+                if ($depot = MedicineDepot::where('upc', $upc)->first()) {
+                    // 品库中有此商品
+
+                    // 创建分类
+                    // 查找该商品在品库中的所有分类ID
+                    $depot_category_ids = \DB::table('wm_depot_medicine_category')->where('medicine_id', $depot->id)->get()->pluck('category_id');
+                    if (!empty($depot_category_ids)) {
+                        // 根据查找的分类ID，查找该商品所有分类
+                        $depot_categories = MedicineDepotCategory::whereIn('id', $depot_category_ids)->get();
+                        if (!empty($depot_categories)) {
+                            foreach ($depot_categories as $depot_category) {
+                                $pid = 0;
+                                if ($depot_category->pid != 0) {
+                                    if ($depot_category_parent = MedicineDepotCategory::find($depot_category->pid)) {
+                                        try {
+                                            $category_parent = MedicineCategory::firstOrCreate(
+                                                [
+                                                    'shop_id' => $this->shop_id,
+                                                    'name' => $depot_category_parent->name,
+                                                ],
+                                                [
+                                                    'shop_id' => $this->shop_id,
+                                                    'pid' => 0,
+                                                    'name' => $depot_category_parent->name,
+                                                    'sort' => $depot_category_parent->sort,
+                                                ]
+                                            );
+                                        } catch (\Exception $exception) {
+                                            $category_parent = MedicineCategory::where([
                                                 'shop_id' => $this->shop_id,
                                                 'name' => $depot_category_parent->name,
-                                            ],
-                                            [
-                                                'shop_id' => $this->shop_id,
-                                                'pid' => 0,
-                                                'name' => $depot_category_parent->name,
-                                                'sort' => $depot_category_parent->sort,
-                                            ]
-                                        );
-                                    } catch (\Exception $exception) {
-                                        $category_parent = MedicineCategory::where([
-                                            'shop_id' => $this->shop_id,
-                                            'name' => $depot_category_parent->name,
-                                        ])->first();
-                                        \Log::info("创建分类失败(父级)|shop_id:{$this->shop_id}|name:{$depot_category_parent->name}|重新查询结果：", [$category_parent]);
+                                            ])->first();
+                                            \Log::info("创建分类失败(父级)|shop_id:{$this->shop_id}|name:{$depot_category_parent->name}|重新查询结果：", [$category_parent]);
+                                        }
+                                        $pid = $category_parent->id;
                                     }
-                                    $pid = $category_parent->id;
                                 }
-                            }
-                            try {
-                                $c = MedicineCategory::firstOrCreate(
-                                    [
+                                try {
+                                    $c = MedicineCategory::firstOrCreate(
+                                        [
+                                            'shop_id' => $this->shop_id,
+                                            'name' => $depot_category->name,
+                                        ],
+                                        [
+                                            'shop_id' => $this->shop_id,
+                                            'pid' => $pid,
+                                            'name' => $depot_category->name,
+                                            'sort' => $depot_category->sort,
+                                        ]
+                                    );
+                                } catch (\Exception $exception) {
+                                    $c = MedicineCategory::where([
                                         'shop_id' => $this->shop_id,
                                         'name' => $depot_category->name,
-                                    ],
-                                    [
-                                        'shop_id' => $this->shop_id,
-                                        'pid' => $pid,
-                                        'name' => $depot_category->name,
-                                        'sort' => $depot_category->sort,
-                                    ]
-                                );
-                            } catch (\Exception $exception) {
-                                $c = MedicineCategory::where([
-                                    'shop_id' => $this->shop_id,
-                                    'name' => $depot_category->name,
-                                ])->first();
-                                \Log::info("创建分类失败|shop_id:{$this->shop_id}|name:{$depot_category->name}|重新查询结果：", [$c]);
+                                    ])->first();
+                                    \Log::info("创建分类失败|shop_id:{$this->shop_id}|name:{$depot_category->name}|重新查询结果：", [$c]);
+                                }
                             }
                         }
                     }
-                }
-                // 创建商品数组
-                $medicine_arr = [
-                    'shop_id' => $this->shop_id,
-                    'name' => $depot->name,
-                    'upc' => $depot->upc,
-                    'cover' => $depot->cover,
-                    'brand' => $depot->brand,
-                    'spec' => $depot->spec,
-                    'price' => $price,
-                    'stock' => $stock,
-                    'guidance_price' => $cost,
-                    'depot_id' => $depot->id,
-                ];
-            } else {
-                // 品库中没有此商品
-                $l = strlen($upc);
-                $name = $this->medicine['name'];
-                if ($l >= 7 && $l <= 19) {
-                    $_depot = MedicineDepot::create([
+                    // 创建商品数组
+                    $medicine_arr = [
+                        'shop_id' => $this->shop_id,
+                        'name' => $depot->name,
+                        'upc' => $depot->upc,
+                        'cover' => $depot->cover,
+                        'brand' => $depot->brand,
+                        'spec' => $depot->spec,
+                        'price' => $price,
+                        'stock' => $stock,
+                        'guidance_price' => $cost,
+                        'depot_id' => $depot->id,
+                        'down_price' => $down_price,
+                        'sequence' => $sequence,
+                        'store_id' => $store_id,
+                    ];
+                } else {
+                    // 品库中没有此商品
+                    $l = strlen($upc);
+                    $name = $this->medicine['name'];
+                    if ($l >= 7 && $l <= 19) {
+                        $_depot = MedicineDepot::create([
+                            'name' => $name,
+                            'upc' => $upc
+                        ]);
+                        \DB::table('wm_depot_medicine_category')->insert(['medicine_id' => $_depot->id, 'category_id' => 215]);
+                    }
+                    $medicine_arr = [
+                        'shop_id' => $this->shop_id,
                         'name' => $name,
-                        'upc' => $upc
-                    ]);
-                    \DB::table('wm_depot_medicine_category')->insert(['medicine_id' => $_depot->id, 'category_id' => 215]);
+                        'upc' => $upc,
+                        'brand' => '',
+                        'spec' => '',
+                        'price' => $price,
+                        'stock' => $stock,
+                        'guidance_price' => $cost,
+                        'depot_id' => 0,
+                        'down_price' => $down_price,
+                        'sequence' => $sequence,
+                        'store_id' => $store_id,
+                    ];
+                    // 创建-暂未分类
+                    $c = MedicineCategory::firstOrCreate(
+                        [
+                            'shop_id' => $this->shop_id,
+                            'name' => '暂未分类',
+                        ],
+                        [
+                            'shop_id' => $this->shop_id,
+                            'pid' => 0,
+                            'name' => '暂未分类',
+                            'sort' => 1000,
+                        ]
+                    );
                 }
-                $medicine_arr = [
-                    'shop_id' => $this->shop_id,
-                    'name' => $name,
-                    'upc' => $upc,
-                    'brand' => '',
-                    'spec' => '',
-                    'price' => $price,
-                    'stock' => $stock,
-                    'guidance_price' => $cost,
-                    'depot_id' => 0,
-                ];
-                // 创建-暂未分类
-                $c = MedicineCategory::firstOrCreate(
-                    [
-                        'shop_id' => $this->shop_id,
-                        'name' => '暂未分类',
-                    ],
-                    [
-                        'shop_id' => $this->shop_id,
-                        'pid' => 0,
-                        'name' => '暂未分类',
-                        'sort' => 1000,
-                    ]
-                );
+                $medicine = Medicine::create($medicine_arr);
+                \DB::table('wm_medicine_category')->insert(['medicine_id' => $medicine->id, 'category_id' => $c->id]);
+            } else {
+                $status = false;
+                $msg = '药品已存在，不能重复添加';
             }
-            $medicine = Medicine::create($medicine_arr);
-            \DB::table('wm_medicine_category')->insert(['medicine_id' => $medicine->id, 'category_id' => $c->id]);
-        } else {
-            $status = false;
         }
+
         $redis_key = 'medicine_zhongtai_add_job_key_' . $this->log_id;
         $redis_key_s = 'medicine_zhongtai_add_job_key_s' . $this->log_id;
         $redis_key_f = 'medicine_zhongtai_add_job_key_f' . $this->log_id;
@@ -188,26 +214,25 @@ class MedicineImportJob implements ShouldQueue
                 'log_id' => $this->log_id,
                 'name' => $this->medicine['name'],
                 'upc' => $this->medicine['upc'],
-                'msg' => $status ? '药品添加成功' : '药品已存在，不能重复添加',
+                'msg' => $msg,
             ]);
         }
-        // \Log::info('查询log之前JOB：' . $this->unique_key, [$this->log_id]);
-        $log = \DB::table('wm_medicine_sync_logs')->where('id', $this->log_id)->first();
-        // \Log::info('查询log之后JOB：' . $this->unique_key, [$log]);
-        if (!$log) {
-            sleep(3);
-            $log = \DB::table('wm_medicine_sync_logs')->where('id', $this->log_id)->first();
-            // \Log::info('重新查询log之后JOB：' . $this->unique_key, [$log]);
-        }
+        // // \Log::info('查询log之前JOB：' . $this->unique_key, [$this->log_id]);
+        // $log = \DB::table('wm_medicine_sync_logs')->where('id', $this->log_id)->first();
+        // // \Log::info('查询log之后JOB：' . $this->unique_key, [$log]);
+        // if (!$log) {
+        //     sleep(3);
+        //     $log = \DB::table('wm_medicine_sync_logs')->where('id', $this->log_id)->first();
+        //     // \Log::info('重新查询log之后JOB：' . $this->unique_key, [$log]);
+        // }
 
-        if ($log->total <= ($log->success + $log->fail + $redis_number_s + $redis_number_f)) {
-            \Log::info('执行一次');
+        if ($this->log_total <= ($redis_number_s + $redis_number_f)) {
             Redis::expire($redis_key, 60);
             Redis::expire($redis_key_s, 60);
             Redis::expire($redis_key_f, 60);
-            $log->update([
-                'success' => $log->success + $redis_number_s,
-                'fail' => $log->fail + $redis_number_f,
+            MedicineSyncLog::where('id', $this->log_id)->update([
+                'success' => $redis_number_s,
+                'fail' => $redis_number_f,
                 'status' => 2,
             ]);
         }
