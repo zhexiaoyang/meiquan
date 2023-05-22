56bfaad67df7ccecc0c51ee5031120f386b47a3d
zhangzhen <zhangzhen@yeah.net>
2023-05-22 08:03:17 +0800

    美全达

--- app/Http/Controllers/Erp/V2/ProductController.php
+++ app/Http/Controllers/Erp/V2/ProductController.php
@@ -64,12 +64,12 @@ class ProductController extends Controller
         if (!$shop = Shop::where($shop_where)->first()) {
             return $this->error("门店不存在。", 701);
         }
-        if (!$key_shop = ErpAccessShop::where(['shop_id' => $shop->id, 'access_id' => $access->id])->first()) {
-            return $this->error("门店不存在", 701);
-        }
-        if (!$key_shop->sync_status) {
+        if (!$shop->sync_status) {
             \Log::info("中台关闭数据同步|shop_id:{$shop->id}");
             return $this->error("中台关闭数据同步", 701);
+        }
+        if (!$key_shop = ErpAccessShop::where(['shop_id' => $shop->id, 'access_id' => $access->id])->first()) {
+            return $this->error("门店不存在", 701);
         }
         \Log::info("ERPV2全部参数门店ID{$shop->id}", $request->all());
 
