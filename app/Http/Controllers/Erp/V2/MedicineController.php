f2610110bc05290991e19a8b73d7e8854c050830
zhangzhen <zhangzhen@yeah.net>
2023-05-24 19:10:41 +0800

    美全达

--- app/Http/Controllers/Erp/V2/MedicineController.php
+++ app/Http/Controllers/Erp/V2/MedicineController.php
@@ -3,30 +3,109 @@
 namespace App\Http\Controllers\Erp\V2;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class MedicineController extends Controller
 {
     public function index(Request $request)
     {
+        $shopId = $request->get('shopId');
+        $type = (int) $request->get('type');
+        $page = $request->get('page');
+        $pageSize = $request->get('pageSize', 100);
+
+        if (!$shopId) {
+            return $this->error('门店ID不能为空', 10010);
+        }
+        if (!$type) {
+            return $this->error('平台不能为空', 10010);
+        }
+        if (!in_array($type, [1, 2])) {
+            return $this->error('平台类型错误', 10010);
+        }
+        if (!$page) {
+            return $this->error('页码不能为空', 10010);
+        }
+        if ($pageSize < 0 || $pageSize > 100) {
+            $pageSize = 100;
+        }
+        $shop = null;
+        if ($type === 1) {
+            $shop = Shop::where('waimai_mt', $shopId)->first();
+        } else {
+            $shop = Shop::where('waimai_ele', $shopId)->first();
+        }
+        if (!$shop) {
+            return $this->error('该门店未绑定', 10010);
+        }
+        if ($type === 1 && !in_array($shop->meituan_bind_platform, [4, 31])) {
+            return $this->error('该门店未绑定!', 10010);
+        }
+        $total = 0;
+        $list = [];
+        if ($type === 1) {
+            $token = false;
+            if ($shop->meituan_bind_platform === 31) {
+                $token = true;
+                $meituan = app('meiquan');
+            } else {
+                $meituan = app('minkang');
+            }
+            $params = [
+                'app_poi_code' => $shop->waimai_mt,
+                'offset' => $pageSize * ($page - 1),
+                'limit' => $pageSize
+            ];
+            $res = $meituan->medicineList($params, $token);
+            if (isset($res['extra_info']['total_count']) && !empty($res['data'])) {
+                $total = $res['extra_info']['total_count'];
+                foreach ($res['data'] as $v) {
+                    $list[] = [
+                        "id" => $v['app_medicine_code'],
+                        "name" => $v['name'],
+                        "storeCode" => $v['app_medicine_code'],
+                        "upc" => $v['upc'],
+                        "spec" => $v['spec'],
+                        "price" => $v['price'],
+                        "stock" => $v['stock'],
+                        "categoryCode" => $v['category_code'],
+                        "categoryName" => $v['category_name'],
+                        "sequence" => $v['sequence'],
+                    ];
+                }
+            }
+        } else {
+            $ele = app('ele');
+            $params = [
+                'shop_id' => $shop->waimai_ele,
+                'page' => $page,
+                'pageSize' => $pageSize,
+            ];
+            $res = $ele->skuList($params);
+            if (!isset($res['total']) && !empty($res['list'])) {
+                $total = $res['total'];
+                foreach ($res['list'] as $v) {
+                    $list[] = [
+                        "id" => $v['custom_sku_id'],
+                        "name" => $v['name'],
+                        "storeCode" => $v['custom_sku_id'],
+                        "upc" => $v['upc'],
+                        "spec" => '',
+                        "price" => $v['sale_price'],
+                        "stock" => $v['left_num'],
+                        "categoryCode" => '',
+                        "categoryName" => '',
+                        "sequence" => 0,
+                    ];
+                }
+            }
+        }
         $res = [
-            'total' => 1,
-            'page' => 1,
-            'pageSize' => 10,
-            'list' => [
-                [
-                    "id" => "7812638", //美团（饿了么）ID
-                    "name" => "布洛芬缓释片", //药品名称
-                    "storeCode" => "code_123",
-                    "upc" => "60392038392302", //药品条码
-                    "spec" => "0.5g*10粒", //规格
-                    "price" => 13.5, //售价
-                    "stock" => 10, //库存
-                    "categoryCode" => "901600",
-                    "categoryName" => "退烧止痛",
-                    "sequence" => 10 //药品在所属末级分类下的排序序号，同一分类下药品排序序号数字越小，前端排位越靠前。
-                ]
-            ]
+            'total' => $total,
+            'page' => $page,
+            'pageSize' => $pageSize,
+            'list' => $list,
         ];
         return $this->success($res);
     }
