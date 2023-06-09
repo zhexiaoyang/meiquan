7dad8a056ec8644d899746c3ccfd191f1aad2bca
zhangzhen <zhangzhen@yeah.net>
2023-06-09 22:52:09 +0800

    美全达

--- app/Models/WmAnalysis.php
+++ app/Models/WmAnalysis.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WmAnalysis extends Model
 {
-    protected $fillable = ['shop_id','platform','sales_volume','order_receipts','order_total_number','product_cost',
+    protected $fillable = ['shop_id','platform','sales_volume','order_receipts','order_total_number','product_cost','operate_service',
         'order_average','running_money','prescription','profit','date','order_effective_number','order_cancel_number'];
 
     public function shop() {
