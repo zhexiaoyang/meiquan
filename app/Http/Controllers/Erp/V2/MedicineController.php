d51c868a90d5c6af643e67e39688d2e6650f0142
zhangzhen <zhangzhen@yeah.net>
2023-06-20 21:08:57 +0800

    美全达

--- app/Http/Controllers/Erp/V2/MedicineController.php
+++ app/Http/Controllers/Erp/V2/MedicineController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Erp\V2;
 
 use App\Http\Controllers\Controller;
+use App\Models\Medicine;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 
@@ -107,36 +108,313 @@ class MedicineController extends Controller
 
     public function add(Request $request)
     {
+        $mt_id = $request->get('shopIdMeiTuan');
+        $ele_id = $request->get('shopIdEle');
+        if (!$mt_id && !$ele_id) {
+            return $this->error('美团ID和饿了么ID至少需要一个');
+        }
+        if (!$store_code = $request->get('storeCode')) {
+            return $this->error('商家商品编码不能为空');
+        }
+        if (!$upc = $request->get('upc')) {
+            return $this->error('商品条码不能为空');
+        }
+        if (!$name = $request->get('name')) {
+            return $this->error('商品名称不能为空');
+        }
+        if (($price = $request->get('price')) === null) {
+            return $this->error('商品价格不能为空');
+        }
+        if (!is_numeric($price)) {
+            return $this->error('商品价格格式不正确');
+        }
+        if ($price <= 0) {
+            return $this->error('商品价格格式不正确。');
+        }
+        if (($stock = $request->get('stock')) === null) {
+            return $this->error('商品库存不能为空');
+        }
+        if (!is_numeric($stock)) {
+            return $this->error('商品库存格式不正确');
+        }
+        if ($stock < 0) {
+            return $this->error('商品库存格式不正确。');
+        }
+        if (!$categoryName = $request->get('categoryName')) {
+            return $this->error('商品分类不能为空');
+        }
+        if (!$status = $request->get('status')) {
+            return $this->error('商品状态不能为空');
+        }
+        if (!in_array($status, [1, 2])) {
+            return $this->error('商品状态格式不正确');
+        }
+        if (($sequence = $request->get('sequence')) === null) {
+            return $this->error('商品排序不能为空');
+        }
+        if (!is_numeric($sequence)) {
+            return $this->error('商品排序格式不正确');
+        }
+        if ($sequence < 0) {
+            return $this->error('商品排序格式不正确。');
+        }
+        $mtCode = 701;
+        $eleCode = 701;
+        $mtMsg = '美团新增商品成功';
+        $eleMsg = '饿了么新增商品成功';
+        if ($mt_id) {
+            if ($shop = Shop::select('id', 'meituan_bind_platform', 'waimai_mt')->where('waimai_mt', $mt_id)->first()) {
+                $meituan = null;
+                if ($shop->meituan_bind_platform === 4) {
+                    $meituan = app('minkang');
+                } elseif ($shop->meituan_bind_platform === 31) {
+                    $meituan = app('meiquan');
+                }
+                if ($meituan) {
+                    $category_params = [
+                        "app_poi_code" => $shop->waimai_mt,
+                        "category_name" => $categoryName,
+                        "sequence" => 100,
+                    ];
+                    if ($shop->meituan_bind_platform === 31) {
+                        $category_params['access_token'] = $meituan->getShopToken($shop->waimai_mt);
+                    }
+                    $meituan->medicineCatSave($category_params);
+                    $params_mt = [
+                        'app_poi_code' => $shop->waimai_mt,
+                        'app_medicine_code' => $store_code,
+                        'upc' => $upc,
+                        'price' => $price,
+                        'stock' => $stock,
+                        'category_name' => $categoryName,
+                        'is_sold_out' => 0,
+                        'sequence' => $sequence
+                    ];
+                    if ($shop->meituan_bind_platform === 31) {
+                        $params_mt['access_token'] = $meituan->getShopToken($shop->waimai_mt);
+                    }
+                    $mt_res = $meituan->medicineSave($params_mt);
+                    if ($mt_res['data'] === 'ok') {
+                        $mtCode = 0;
+                        $mtMsg = '美团成功';
+                    } else {
+                        $mtCode = 702;
+                        $mtMsg = $create_log['error']['msg'] ?? '美团失败';
+                    }
+                }
+            } else {
+                $mtMsg = '门店不存在';
+            }
+        }
+        if ($ele_id) {
+            $ele = app('ele');
+            $params_ele = [
+                'shop_id' => $ele_id,
+                'name' => $name,
+                'upc' => $upc,
+                'custom_sku_id' => $store_code,
+                'sale_price' => (int) ($price * 100),
+                'left_num' => $stock,
+                'category_name' => $categoryName,
+                'status' => 1,
+                'base_rec_enable' => true,
+                'photo_rec_enable' => true,
+                'summary_rec_enable' => true,
+                'cat_prop_rec_enable' => true,
+            ];
+            $res = $ele->add_product($params_ele);
+            if ($res['body']['error'] === 'success') {
+                $eleCode = 0;
+                $eleMsg = '饿了么成功';
+            } else {
+                $eleCode = 702;
+                $eleMsg = $res['body']['error'] ?? '';
+            }
+        }
         $res = [
-            "meituanCode" => 0, //美团新增状态码（0 成功，其它失败）
-            "meituanMessage" => "美团新增商品成功", //美团返回信息描述
-            "eleCode" => 701, //饿了么新增状态码（0 成功，其它失败）
-            "eleMessage" => "饿了么新增失败：分类不存在" //饿了么返回信息描述
+            "meituanCode" => $mtCode, //美团新增状态码（0 成功，其它失败）
+            "meituanMessage" => $mtMsg, //美团返回信息描述
+            "eleCode" => $eleCode, //饿了么新增状态码（0 成功，其它失败）
+            "eleMessage" => $eleMsg //饿了么返回信息描述
         ];
         return $this->success($res);
     }
 
-    public function update()
+    public function update(Request $request)
     {
+        $mt_id = $request->get('shopIdMeiTuan');
+        $ele_id = $request->get('shopIdEle');
+        if (!$mt_id && !$ele_id) {
+            return $this->error('美团ID和饿了么ID至少需要一个');
+        }
+        if (!$store_code = $request->get('storeCode')) {
+            return $this->error('商家商品编码不能为空');
+        }
+        if (($price = $request->get('price')) === null) {
+            return $this->error('商品价格不能为空');
+        }
+        if (!is_numeric($price)) {
+            return $this->error('商品价格格式不正确');
+        }
+        if ($price <= 0) {
+            return $this->error('商品价格格式不正确。');
+        }
+        if (($stock = $request->get('stock')) === null) {
+            return $this->error('商品库存不能为空');
+        }
+        if (!is_numeric($stock)) {
+            return $this->error('商品库存格式不正确');
+        }
+        if ($stock < 0) {
+            return $this->error('商品库存格式不正确。');
+        }
+        if (!$status = $request->get('status')) {
+            return $this->error('商品状态不能为空');
+        }
+        if (!in_array($status, [1, 2])) {
+            return $this->error('商品状态格式不正确');
+        }
+        if (($sequence = $request->get('sequence')) === null) {
+            return $this->error('商品排序不能为空');
+        }
+        if (!is_numeric($sequence)) {
+            return $this->error('商品排序格式不正确');
+        }
+        if ($sequence < 0) {
+            return $this->error('商品排序格式不正确。');
+        }
+        $mtCode = 701;
+        $eleCode = 701;
+        $mtMsg = '美团更新商品成功';
+        $eleMsg = '饿了么更新商品成功';
+        if ($mt_id) {
+            if ($shop = Shop::select('id', 'meituan_bind_platform', 'waimai_mt')->where('waimai_mt', $mt_id)->first()) {
+                $meituan = null;
+                if ($shop->meituan_bind_platform === 4) {
+                    $meituan = app('minkang');
+                } elseif ($shop->meituan_bind_platform === 31) {
+                    $meituan = app('meiquan');
+                }
+                if ($meituan) {
+                    $params_mt = [
+                        'app_poi_code' => $shop->waimai_mt,
+                        'app_medicine_code' => $store_code,
+                        'price' => $price,
+                        'stock' => $stock,
+                        'is_sold_out' => 0,
+                        'sequence' => $sequence
+                    ];
+                    if ($shop->meituan_bind_platform === 31) {
+                        $params_mt['access_token'] = $meituan->getShopToken($shop->waimai_mt);
+                    }
+                    $mt_res = $meituan->medicineUpdate($params_mt);
+                    if ($mt_res['data'] === 'ok') {
+                        $mtCode = 0;
+                        $mtMsg = '美团成功';
+                    } else {
+                        $mtCode = 702;
+                        $mtMsg = $create_log['error']['msg'] ?? '美团失败';
+                    }
+                }
+            } else {
+                $mtMsg = '门店不存在';
+            }
+        }
+        if ($ele_id) {
+            $ele = app('ele');
+            $params_ele = [
+                'shop_id' => $ele_id,
+                'custom_sku_id' => $store_code,
+                'sale_price' => (int) ($price * 100),
+                'left_num' => $stock,
+                'status' => 1,
+                'base_rec_enable' => true,
+                'photo_rec_enable' => true,
+                'summary_rec_enable' => true,
+                'cat_prop_rec_enable' => true,
+            ];
+            $res = $ele->skuUpdate($params_ele);
+            if ($res['body']['error'] === 'success') {
+                $eleCode = 0;
+                $eleMsg = '饿了么成功';
+            } else {
+                $eleCode = 702;
+                $eleMsg = $res['body']['error'] ?? '';
+            }
+        }
         $res = [
-            "meituanCode" => 0, //美团新增状态码（0 成功，其它失败）
-            "meituanMessage" => "美团修改商品成功", //美团返回信息描述
-            "eleCode" => 701, //饿了么新增状态码（0 成功，其它失败）
-            "eleMessage" => "饿了么修改失败：分类不存在" //饿了么返回信息描述
+            "meituanCode" => $mtCode, //美团新增状态码（0 成功，其它失败）
+            "meituanMessage" => $mtMsg, //美团返回信息描述
+            "eleCode" => $eleCode, //饿了么新增状态码（0 成功，其它失败）
+            "eleMessage" => $eleMsg //饿了么返回信息描述
         ];
-
         return $this->success($res);
     }
 
-    public function delete()
+    public function delete(Request $request)
     {
+        $mt_id = $request->get('shopIdMeiTuan');
+        $ele_id = $request->get('shopIdEle');
+        if (!$mt_id && !$ele_id) {
+            return $this->error('美团ID和饿了么ID至少需要一个');
+        }
+        if (!$store_code = $request->get('storeCode')) {
+            return $this->error('商家商品编码不能为空');
+        }
+        $mtCode = 701;
+        $eleCode = 701;
+        $mtMsg = '美团删除商品成功';
+        $eleMsg = '饿了么删除商品成功';
+        if ($mt_id) {
+            if ($shop = Shop::select('id', 'meituan_bind_platform', 'waimai_mt')->where('waimai_mt', $mt_id)->first()) {
+                $meituan = null;
+                if ($shop->meituan_bind_platform === 4) {
+                    $meituan = app('minkang');
+                } elseif ($shop->meituan_bind_platform === 31) {
+                    $meituan = app('meiquan');
+                }
+                if ($meituan) {
+                    $params_mt = [
+                        'app_poi_code' => $shop->waimai_mt,
+                        'app_medicine_code' => $store_code,
+                    ];
+                    // if ($shop->meituan_bind_platform === 31) {
+                    //     $params_mt['access_token'] = $meituan->getShopToken($shop->waimai_mt);
+                    // }
+                    $mt_res = $meituan->medicineDelete($params_mt);
+                    if ($mt_res['data'] === 'ok') {
+                        $mtCode = 0;
+                        $mtMsg = '美团成功';
+                    } else {
+                        $mtCode = 702;
+                        $mtMsg = $create_log['error']['msg'] ?? '美团失败';
+                    }
+                }
+            } else {
+                $mtMsg = '门店不存在';
+            }
+        }
+        if ($ele_id) {
+            $ele = app('ele');
+            $params_ele = [
+                'shop_id' => $ele_id,
+                'custom_sku_id' => (string) $store_code,
+            ];
+            $res = $ele->skuDelete($params_ele);
+            if ($res['body']['error'] === 'success') {
+                $eleCode = 0;
+                $eleMsg = '饿了么成功';
+            } else {
+                $eleCode = 702;
+                $eleMsg = $res['body']['error'] ?? '';
+            }
+        }
         $res = [
-            "meituanCode" => 0, //美团新增状态码（0 成功，其它失败）
-            "meituanMessage" => "美团删除商品成功", //美团返回信息描述
-            "eleCode" => 701, //饿了么新增状态码（0 成功，其它失败）
-            "eleMessage" => "饿了么删除失败：药品不存在" //饿了么返回信息描述
+            "meituanCode" => $mtCode, //美团新增状态码（0 成功，其它失败）
+            "meituanMessage" => $mtMsg, //美团返回信息描述
+            "eleCode" => $eleCode, //饿了么新增状态码（0 成功，其它失败）
+            "eleMessage" => $eleMsg //饿了么返回信息描述
         ];
-
         return $this->success($res);
     }
 }
--- app/Http/Controllers/Erp/V2/OrderController.php
+++ app/Http/Controllers/Erp/V2/OrderController.php
@@ -3,40 +3,157 @@
 namespace App\Http\Controllers\Erp\V2;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function info(Request $request)
     {
-        $res = [
-            "orderId" => "800523467812378",
-            "isPrescription" => 0,
-            "recipientName" => "测试（先生）",
-            "recipientPhone" => "13812345678_1236",
-            "recipientAddress" => "色金拉 (色金拉)",
-            "shippingFee" => 4,
-            "total" => 4,
-            "originalPrice" => 4,
-            "caution" => "【如遇缺货】：缺货时电话与我沟通收货人隐私号 18689114387_3473,手机号 185****2033",
-            "status" => 4,
-            "ctime" => "1558955579",
-            "utime" => "1558955579",
-            "latitude" => 29.774491,
-            "longitude" => 95.369272,
-            "daySeq" => 12,
-            "detail" => [
-                [
-                    "storeCode" => "code_24367",
-                    "name" => "温度计",
-                    "upc" => "6923995006311",
-                    "quantity" => 2,
-                    "price" => 12.5,
-                    "unit" => "售卖单位",
-                    "spec" => "规格",
-                ]
-            ]
-        ];
+        $mt_id = $request->get('shopIdMeiTuan');
+        $ele_id = $request->get('shopIdEle');
+        if (!$mt_id && !$ele_id) {
+            return $this->error('美团ID和饿了么ID至少需要一个');
+        }
+        if (!$order_id = $request->get('orderId')) {
+            return $this->error('订单号不能为空');
+        }
+
+        $res = [];
+        if ($mt_id) {
+            if (!$shop = Shop::select('id', 'meituan_bind_platform', 'waimai_mt')->where('waimai_mt', $mt_id)->first()) {
+                return $this->error('门店不存在');
+            }
+            $meituan = null;
+            if ($shop->meituan_bind_platform === 4) {
+                $meituan = app('minkang');
+            } elseif ($shop->meituan_bind_platform === 31) {
+                $meituan = app('meiquan');
+            }
+            if ($meituan) {
+                $params_mt = [
+                    'order_id' => $order_id,
+                ];
+                $mt_res = $meituan->getOrderDetail($params_mt);
+                if (!empty($mt_res) && is_array($mt_res['data']) && !empty($mt_res['data'])) {
+                    $mt_data = $mt_res['data'];
+                    // 是否处方
+                    $order_tag_list = json_decode(urldecode($mt_data['order_tag_list']), true);
+                    $is_prescription = 0;
+                    if (in_array(8, $order_tag_list)) {
+                        $is_prescription = 1;
+                    }
+                    $res = [
+                        "orderId" => $mt_data['order_id'],
+                        "isPrescription" => $is_prescription,
+                        "recipientName" => $mt_data['recipient_name'],
+                        "recipientPhone" => $mt_data['recipient_phone'],
+                        "recipientAddress" => $mt_data['recipient_address'],
+                        "shippingFee" => $mt_data['shipping_fee'],
+                        "total" => $mt_data['total'],
+                        "originalPrice" => $mt_data['original_price'],
+                        "caution" => $mt_data['caution'],
+                        "status" => $mt_data['status'],
+                        "ctime" => $mt_data['ctime'],
+                        "utime" => $mt_data['utime'],
+                        "latitude" => $mt_data['latitude'],
+                        "longitude" => $mt_data['longitude'],
+                        "daySeq" => $mt_data['day_seq'],
+                        "detail" => []
+                    ];
+                    $products = json_decode($mt_data['detail'], true);
+                    $detail = [];
+                    if (!empty($products)) {
+                        foreach ($products as $product) {
+                            $detail[] = [
+                                "storeCode" => $product['app_food_code'],
+                                "skuId" => $product['sku_id'],
+                                "name" => $product['food_name'],
+                                "upc" => $product['upc'] ?? '',
+                                "quantity" => $product['quantity'],
+                                "price" => $product['price'],
+                                "unit" => $product['unit'],
+                                "spec" => $product['spec'],
+                            ];
+                        }
+                    }
+                    $res['detail'] = $detail;
+                }
+            }
+        } else {
+            $ele = app('ele');
+            $order_request = $ele->orderInfo($order_id);
+            if (!empty($order_request) && isset($order_request['body']['data']) && !empty($order_request['body']['data'])) {
+                // 订单数组
+                $ele_data = $order_request['body']['data'];
+                $products = $ele_data['products'];
+                // 饿了么订单状态： 1 待确认，5 已确认，7 骑士已接单，8 骑士已取餐，快递已揽收（快递发货场景），9 已完成，10 已取消
+                // 美团订单状态：返回订单当前的状态。目前平台的订单状态参考值有：1-用户已提交订单；2-向商家推送订单；4-商家已确认；8-订单已完成；9-订单已取消。
+                $status_map = [1 => 1, 5 => 4, 7 => 4, 8 => 4, 9 => 8, 10 => 9];
+                $res = [
+                    "orderId" => $ele_data['order']['order_id'],
+                    "isPrescription" => $ele_data['order']['is_prescription'],
+                    "recipientName" => empty($ele_data['user']['name']) ? "无名客人" : $ele_data['user']['name'],
+                    "recipientPhone" => str_replace(',', '_', $ele_data['user']['phone']),
+                    "recipientAddress" => $ele_data['user']['address'],
+                    "shippingFee" => $ele_data['order']['send_fee'] / 100,
+                    "total" => $ele_data['order']['user_fee'] / 100,
+                    "originalPrice" => $ele_data['order']['total_fee'] / 100,
+                    "caution" => $ele_data['order']['remark'] ?: '',
+                    "status" => $status_map[$ele_data['order']['status']],
+                    "ctime" => $ele_data['order']['create_time'],
+                    "utime" => $ele_data['order']['create_time'],
+                    "latitude" => $ele_data['user']['coord_amap']['latitude'],
+                    "longitude" => $ele_data['user']['coord_amap']['longitude'],
+                    "daySeq" => $ele_data['order']['order_index'] ?? 0,
+                    "detail" => []
+                ];
+                $detail = [];
+                if (!empty($products)) {
+                    foreach ($products as $product) {
+                        $detail[] = [
+                            "storeCode" => $product['custom_sku_id'],
+                            "skuId" => $product['custom_sku_id'],
+                            "name" => $product['product_name'],
+                            "upc" => $product['upc'] ?? '',
+                            "quantity" => $product['product_amount'],
+                            "price" => $product['product_price'] / 100,
+                            "unit" => '',
+                            "spec" => '',
+                        ];
+                    }
+                }
+                $res['detail'] = $detail;
+            }
+        }
+        // $res = [
+        //     "orderId" => "800523467812378",
+        //     "isPrescription" => 0,
+        //     "recipientName" => "测试（先生）",
+        //     "recipientPhone" => "13812345678_1236",
+        //     "recipientAddress" => "色金拉 (色金拉)",
+        //     "shippingFee" => 4,
+        //     "total" => 4,
+        //     "originalPrice" => 4,
+        //     "caution" => "【如遇缺货】：缺货时电话与我沟通收货人隐私号 18689114387_3473,手机号 185****2033",
+        //     "status" => 4,
+        //     "ctime" => "1558955579",
+        //     "utime" => "1558955579",
+        //     "latitude" => 29.774491,
+        //     "longitude" => 95.369272,
+        //     "daySeq" => 12,
+        //     "detail" => [
+        //         [
+        //             "storeCode" => "code_24367",
+        //             "name" => "温度计",
+        //             "upc" => "6923995006311",
+        //             "quantity" => 2,
+        //             "price" => 12.5,
+        //             "unit" => "售卖单位",
+        //             "spec" => "规格",
+        //         ]
+        //     ]
+        // ];
         return $this->success($res);
     }
 
