f798acff68d1fd96e05e368a50c309b4caa18d9c
zhangzhen <zhangzhen@yeah.net>
2023-08-02 14:33:46 +0800

    美全达

--- app/Models/OrderDeliveryTrack.php
+++ app/Models/OrderDeliveryTrack.php
@@ -20,7 +20,7 @@ class OrderDeliveryTrack extends Model
 
 ';
 
-    protected $fillable = ['delivery_id','order_id','wm_id','status','status_desc','delivery_name','delivery_phone',
+    protected $fillable = ['delivery_id','order_id','wm_id','status','status_des','delivery_name','delivery_phone',
         'delivery_lng','delivery_lat','description'];
 
 }
