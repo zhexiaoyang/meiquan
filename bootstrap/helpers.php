6e4ce384864254760edfb1d82a45a165dfbb40fa
zhangzhen <zhangzhen@yeah.net>
2023-08-05 11:09:28 +0800

    美全达

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,42 @@
 <?php
 
+function tranTime($time) {
+    $rtime = date("m-d H:i",$time);
+    $htime = date("H:i",$time);
+
+    $time = time() - $time;
+
+    if ($time < 60)
+    {
+        $str = '刚刚';
+    }
+    elseif ($time < 60 * 60)
+    {
+        $min = floor($time/60);
+        $str = $min.'分钟前';
+    }
+    elseif ($time < 60 * 60 * 24)
+    {
+        $h = floor($time/(60*60));
+        $str = $h.'小时前 '.$htime;
+    }
+    elseif ($time < 60 * 60 * 24 * 3)
+    {
+        $d = floor($time/(60*60*24));
+        if($d==1)
+            // $str = '昨天 '.$rtime;
+            $str = '昨天';
+        else
+            // $str = '前天 '.$rtime;
+            $str = '前天';
+    }
+    else
+    {
+        $str = $rtime;
+    }
+    return $str;
+}
+
 function randomPassword($length = 6)
 {
     //字符组合
