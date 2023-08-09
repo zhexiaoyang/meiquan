695be1a73c039f9d5e84c4da8a902f7fd21ef800
zhangzhen <zhangzhen@yeah.net>
2023-08-09 11:42:22 +0800

    美全达

--- routes/delivery.php
+++ routes/delivery.php
@@ -14,9 +14,9 @@ Route::middleware(['force-json'])->prefix("app")->namespace("Delivery\V1")->grou
             Route::get("index", "OrderController@index");
             Route::get("info", "OrderController@show");
             Route::get("calculate", "OrderController@calculate");
-            Route::get("send", "OrderController@send");
-            Route::get("cancel", "OrderController@cancel");
-            Route::get("ignore", "OrderController@ignore");
+            Route::post("send", "OrderController@send");
+            Route::post("cancel", "OrderController@cancel");
+            Route::post("ignore", "OrderController@ignore");
         });
         // 门店
         Route::prefix('shop')->group(function () {
