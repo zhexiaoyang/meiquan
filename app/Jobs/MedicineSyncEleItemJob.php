43d93cd40aefa45b03972961a6b994550dbc9742
zhangzhen <zhangzhen@yeah.net>
2023-08-15 11:10:01 +0800

    美全达

--- app/Jobs/MedicineSyncEleItemJob.php
+++ app/Jobs/MedicineSyncEleItemJob.php
@@ -65,13 +65,13 @@ class MedicineSyncEleItemJob implements ShouldQueue
             // $this->log('创建药品返回', [$res]);
             $res = $ele->add_product($this->params);
             if ($res['body']['error'] === 'success') {
-                if (Medicine::where('id', $this->medicine_id)->update(['ele_status' => 1, 'online_ele' => 1])) {
+                if (Medicine::where('id', $this->medicine_id)->update(['ele_status' => 1])) {
                     $status = true;
                 }
             } else {
                 $error_msg = $res['body']['error'] ?? '';
                 if ((strpos($error_msg, '已存在') !== false) || (strpos($error_msg, '已经存在') !== false)) {
-                    $update_data = ['ele_status' => 1, 'online_ele' => 1];
+                    $update_data = ['ele_status' => 1];
                     // 库存大于0 为上架状态
                     // if ($this->params['left_num'] > 0) {
                     //     $update_data['online_ele'] = 1;
