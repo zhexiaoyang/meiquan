54d825fcae5ee57fbed7c0257e67c88795f7ef5a
zhangzhen <zhangzhen@yeah.net>
2023-08-18 08:51:36 +0800

    美全达

--- app/Http/Controllers/Erp/V2/OrderController.php
+++ app/Http/Controllers/Erp/V2/OrderController.php
@@ -98,7 +98,7 @@ class OrderController extends Controller
 
         $res = [];
         if ($mt_id) {
-            if (!$shop = Shop::select('id', 'meituan_bind_platform', 'waimai_mt')->where('waimai_mt', $mt_id)->first()) {
+            if (!$shop = Shop::select('id','meituan_bind_platform','waimai_mt','commission_mt')->where('waimai_mt', $mt_id)->first()) {
                 return $this->error('门店不存在');
             }
             $meituan = null;
@@ -117,8 +117,17 @@ class OrderController extends Controller
                     $poi_receive_detail_yuan = json_decode(urldecode($mt_data['poi_receive_detail_yuan']), true);
                     // 是否处方
                     $order_tag_list = $mt_data['order_tag_list'];
+                    $prescription_fee = 0;
                     if (!is_array($order_tag_list)) {
                         $order_tag_list = json_decode(urldecode($mt_data['order_tag_list']), true);
+                        $prescription_fee = 0.8;
+                        $reconciliationExtras = json_decode($poi_receive_detail_yuan['reconciliationExtras'] ?? '', true);
+                        $platformChargeFee2 = (float) $reconciliationExtras['platformChargeFee2'] ?? null;
+                        if (!is_null($platformChargeFee2)) {
+                            if ($platformChargeFee2 == 0.6) {
+                                $prescription_fee = 0.2;
+                            }
+                        }
                     }
                     $is_prescription = 0;
                     if (in_array(8, $order_tag_list)) {
@@ -134,7 +143,6 @@ class OrderController extends Controller
                         "total" => $mt_data['total'],
                         "originalPrice" => $mt_data['original_price'],
                         "poiReceive" => $poi_receive_detail_yuan['poiReceive'] ?? 0,
-                        "keepAccount" => 0,
                         "caution" => $mt_data['caution'],
                         "status" => $mt_data['status'],
                         "ctime" => $mt_data['ctime'],
@@ -142,12 +150,15 @@ class OrderController extends Controller
                         "latitude" => $mt_data['latitude'],
                         "longitude" => $mt_data['longitude'],
                         "daySeq" => $mt_data['day_seq'],
-                        "detail" => []
+                        "keepAccount" => 0,
                     ];
                     $products = json_decode($mt_data['detail'], true);
                     $detail = [];
+                    // 所有商品价格总和
+                    $product_total = 0;
                     if (!empty($products)) {
                         foreach ($products as $product) {
+                            $product_total += $product['price'] * $product['quantity'];
                             $detail[] = [
                                 "storeCode" => $product['app_food_code'],
                                 "skuId" => $product['sku_id'],
@@ -157,26 +168,69 @@ class OrderController extends Controller
                                 "price" => $product['price'],
                                 "unit" => $product['unit'],
                                 "spec" => $product['spec'],
+                                "total_price" => (float) sprintf("%.2f", $product['price'] * $product['quantity']),
                                 "keepAccount" => 0,
                             ];
                         }
                     }
+                    // ------------------下账金额----------------
+                    $poi_receive_detail_yuan = json_decode($mt_data['poi_receive_detail_yuan'], true);
+                    // 代运营服务费
+                    $operate_service_fee = ($shop->commission_mt * $poi_receive_detail_yuan['poiReceive'] ?? 0) / 100;
+                    // 佣金，单位为元
+                    $foodShareFeeChargeByPoi = $poi_receive_detail_yuan['foodShareFeeChargeByPoi'];
+                    // 配送费，单位为元
+                    $logisticsFee = $poi_receive_detail_yuan['logisticsFee'];
+                    // 美团承担
+                    $actOrderChargeByMt = 0;
+                    // 商家承担
+                    $actOrderChargeByPoi = 0;
+                    if (!empty($poi_receive_detail_yuan['actOrderChargeByMt'])) {
+                        foreach ($poi_receive_detail_yuan['actOrderChargeByMt'] as $item) {
+                            $actOrderChargeByMt += $item['money'];
+                        }
+                    }
+                    if (!empty($poi_receive_detail_yuan['actOrderChargeByPoi'])) {
+                        foreach ($poi_receive_detail_yuan['actOrderChargeByPoi'] as $item) {
+                            $actOrderChargeByPoi += $item['money'];
+                        }
+                    }
+                    // 订单下帐金额 = 所有商品原价总金额 - 佣金 - 代运营费 -处方费 - 商家活动承担 + 平台活动承担 + 平台返配送费
+                    $keepAccount = $product_total - $foodShareFeeChargeByPoi - $operate_service_fee - $prescription_fee - $actOrderChargeByPoi + $actOrderChargeByMt + $logisticsFee;
+                    $keepAccountText = "{$order_id}订单下帐金额 = 所有商品原价总金额($product_total) - 佣金($foodShareFeeChargeByPoi) - 代运营费($operate_service_fee) -处方费($prescription_fee) - 商家活动承担($actOrderChargeByPoi) + 平台活动承担($actOrderChargeByMt) + 平台返配送费($logisticsFee)";
+                    // \Log::info($keepAccountText);
+                    if (!empty($detail)) {
+                        foreach ($detail as $k => $v) {
+                            $ratio = $v['total_price'] / $product_total;
+                            $productKeepAccount = round($keepAccount * $ratio, 2);
+                            $productKeepAccountText = $v['name'] . ",商品下账金额 = 订单下帐金额($keepAccount) * 单个商品分摊比($ratio)";
+                            $detail[$k]['keepAccount'] = (float) sprintf("%.2f", $productKeepAccount);
+                            $detail[$k]['keepAccountText'] = $productKeepAccountText;
+                        }
+                    }
+                    $res['keepAccount'] = (float) sprintf("%.2f", $keepAccount);
+                    $res['keepAccountText'] = $keepAccountText;
                     $res['detail'] = $detail;
                 }
             }
         } else {
+            if (!$shop = Shop::select('id','waimai_ele','prescription_cost_ele','commission_mt')->where('waimai_ele', $ele_id)->first()) {
+                return $this->error('门店不存在');
+            }
             $ele = app('ele');
             $order_request = $ele->orderInfo($order_id);
             if (!empty($order_request) && isset($order_request['body']['data']) && !empty($order_request['body']['data'])) {
                 // 订单数组
                 $ele_data = $order_request['body']['data'];
                 $products = $ele_data['products'];
+                $is_prescription = $ele_data['order']['is_prescription'];
+                $prescription_fee = $is_prescription ? $shop->prescription_cost_ele : 0;
                 // 饿了么订单状态： 1 待确认，5 已确认，7 骑士已接单，8 骑士已取餐，快递已揽收（快递发货场景），9 已完成，10 已取消
                 // 美团订单状态：返回订单当前的状态。目前平台的订单状态参考值有：1-用户已提交订单；2-向商家推送订单；4-商家已确认；8-订单已完成；9-订单已取消。
                 $status_map = [1 => 1, 5 => 4, 7 => 4, 8 => 4, 9 => 8, 10 => 9];
                 $res = [
                     "orderId" => $ele_data['order']['order_id'],
-                    "isPrescription" => $ele_data['order']['is_prescription'],
+                    "isPrescription" => $is_prescription,
                     "recipientName" => empty($ele_data['user']['name']) ? "无名客人" : $ele_data['user']['name'],
                     "recipientPhone" => str_replace(',', '_', $ele_data['user']['phone']),
                     "recipientAddress" => $ele_data['user']['address'],
@@ -192,12 +246,14 @@ class OrderController extends Controller
                     "latitude" => $ele_data['user']['coord_amap']['latitude'],
                     "longitude" => $ele_data['user']['coord_amap']['longitude'],
                     "daySeq" => $ele_data['order']['order_index'] ?? 0,
-                    "detail" => []
                 ];
                 $detail = [];
+                $product_total = 0;
                 if (!empty($products)) {
                     foreach ($products as $product_bag) {
                         foreach ($product_bag as $product) {
+                            // $product_total += ($product['product_fee'] + $product['package_fee']) / 100;
+                            $product_total += ($product['product_fee']) / 100;
                             $detail[] = [
                                 "storeCode" => $product['custom_sku_id'],
                                 "skuId" => $product['custom_sku_id'],
@@ -207,42 +263,39 @@ class OrderController extends Controller
                                 "price" => $product['product_price'] / 100,
                                 "unit" => '',
                                 "spec" => '',
+                                "total_price" => ($product['product_fee']) / 100,
                                 "keepAccount" => 0,
                             ];
                         }
                     }
                 }
+                // 代运营服务费
+                $operate_service_fee = ($shop->commission_ele * $ele_data['order']['shop_fee'] / 100) / 100;
+                // 佣金
+                $commission = $ele_data['order']['commission'] / 100;
+                // 商家活动承担
+                $shop_rate = $ele_data['discount']['shop_rate'] / 100;
+                // 平台活动承担
+                $baidu_rate = $ele_data['discount']['baidu_rate'] / 100;
+                // 配送费
+                $send_fee = $ele_data['order']['send_fee'] / 100;
+                // 订单下帐金额 = 所有商品原价总金额 - 佣金 - 代运营费 -处方费 - 商家活动承担 + 平台活动承担 + 平台返配送费
+                $keepAccount = $product_total - $commission - $operate_service_fee - $prescription_fee - $shop_rate + $baidu_rate + $send_fee;
+                $keepAccountText = "{$order_id}订单下帐金额 = 所有商品原价总金额($product_total) - 佣金($commission) - 代运营费($operate_service_fee) -处方费($prescription_fee) - 商家活动承担($shop_rate) + 平台活动承担($baidu_rate) + 平台返配送费($send_fee)";
+                if (!empty($detail)) {
+                    foreach ($detail as $k => $v) {
+                        $ratio = $v['total_price'] / $product_total;
+                        $productKeepAccount = round($keepAccount * $ratio, 2);
+                        $productKeepAccountText = $v['name'] . ",商品下账金额 = 订单下帐金额($keepAccount) * 单个商品分摊比($ratio)";
+                        $detail[$k]['keepAccount'] = (float) sprintf("%.2f", $productKeepAccount);
+                        $detail[$k]['keepAccountText'] = $productKeepAccountText;
+                    }
+                }
+                $res['keepAccount'] = (float) sprintf("%.2f", $keepAccount);
+                $res['keepAccountText'] = $keepAccountText;
                 $res['detail'] = $detail;
             }
         }
-        // $res = [
-        //     "orderId" => "800523467812378",
-        //     "isPrescription" => 0,
-        //     "recipientName" => "测试（先生）",
-        //     "recipientPhone" => "13812345678_1236",
-        //     "recipientAddress" => "色金拉 (色金拉)",
-        //     "shippingFee" => 4,
-        //     "total" => 4,
-        //     "originalPrice" => 4,
-        //     "caution" => "【如遇缺货】：缺货时电话与我沟通收货人隐私号 18689114387_3473,手机号 185****2033",
-        //     "status" => 4,
-        //     "ctime" => "1558955579",
-        //     "utime" => "1558955579",
-        //     "latitude" => 29.774491,
-        //     "longitude" => 95.369272,
-        //     "daySeq" => 12,
-        //     "detail" => [
-        //         [
-        //             "storeCode" => "code_24367",
-        //             "name" => "温度计",
-        //             "upc" => "6923995006311",
-        //             "quantity" => 2,
-        //             "price" => 12.5,
-        //             "unit" => "售卖单位",
-        //             "spec" => "规格",
-        //         ]
-        //     ]
-        // ];
         return $this->success($res);
     }
 
