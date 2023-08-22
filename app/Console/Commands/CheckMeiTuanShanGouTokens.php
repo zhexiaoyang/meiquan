1e02a274a68bbec77cf8368a8506efaae1862b7a
zhangzhen <zhangzhen@yeah.net>
2023-08-22 11:11:51 +0800

    美全达

--- app/Console/Commands/CheckMeiTuanShanGouTokens.php
+++ app/Console/Commands/CheckMeiTuanShanGouTokens.php
@@ -48,17 +48,23 @@ class CheckMeiTuanShanGouTokens extends Command
                 $key = 'mtwm:shop:auth:' . $shop_id;
                 $key_ref = 'mtwm:shop:auth:ref:' . $shop_id;
                 $res = $meituan->waimaiAuthorizeRef($token->refresh_token);
-                $access_token = $res['access_token'];
-                $refresh_token = $res['refresh_token'];
-                $expires_in = $res['expires_in'];
-                Cache::put($key, $access_token, 2505600);
-                Cache::put($key_ref, $refresh_token, 7689600);
-                MeituanShangouToken::where('shop_id', $shop_id)->update([
-                    'access_token' => $res['access_token'],
-                    'refresh_token' => $res['refresh_token'],
-                    'expires_at' => date("Y-m-d H:i:s", time() + $expires_in),
-                    'expires_in' => $expires_in,
-                ]);
+                $this->info('返回：：' . json_encode($res, true));
+                if (isset($res['status']) && $res['status'] == 0) {
+                    $access_token = $res['access_token'];
+                    $refresh_token = $res['refresh_token'];
+                    $expires_in = $res['expires_in'];
+                    Cache::put($key, $access_token, 2505600);
+                    Cache::put($key_ref, $refresh_token, 7689600);
+                    MeituanShangouToken::where('shop_id', $shop_id)->update([
+                        'access_token' => $res['access_token'],
+                        'refresh_token' => $res['refresh_token'],
+                        'expires_at' => date("Y-m-d H:i:s", time() + $expires_in),
+                        'expires_in' => $expires_in,
+                    ]);
+                    $this->info('---成功');
+                } else {
+                    $this->info('---失败');
+                }
                 sleep(5);
             }
         }
