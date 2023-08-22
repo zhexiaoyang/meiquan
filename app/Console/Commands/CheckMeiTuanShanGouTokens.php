56aed7489276428c73c821b2ddafd2f638f1f886
zhangzhen <zhangzhen@yeah.net>
2023-08-22 11:09:18 +0800

    美全达

---
+++ app/Console/Commands/CheckMeiTuanShanGouTokens.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\MeituanShangouToken;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class CheckMeiTuanShanGouTokens extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check-meituan-shangou-tokens';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tokens = MeituanShangouToken::get();
+        if (!empty($tokens)) {
+            $meituan = app('meiquan');
+            foreach ($tokens as $token) {
+                $this->info(json_encode($token, true));
+                $shop_id = $token->shop_id;
+                $key = 'mtwm:shop:auth:' . $shop_id;
+                $key_ref = 'mtwm:shop:auth:ref:' . $shop_id;
+                $res = $meituan->waimaiAuthorizeRef($token->refresh_token);
+                $access_token = $res['access_token'];
+                $refresh_token = $res['refresh_token'];
+                $expires_in = $res['expires_in'];
+                Cache::put($key, $access_token, 2505600);
+                Cache::put($key_ref, $refresh_token, 7689600);
+                MeituanShangouToken::where('shop_id', $shop_id)->update([
+                    'access_token' => $res['access_token'],
+                    'refresh_token' => $res['refresh_token'],
+                    'expires_at' => date("Y-m-d H:i:s", time() + $expires_in),
+                    'expires_in' => $expires_in,
+                ]);
+                sleep(5);
+            }
+        }
+    }
+}
