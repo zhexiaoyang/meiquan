6e0b0bc01824e7ec0260f78796b023bf4281e01d
zhangzhen <zhangzhen@yeah.net>
2023-08-27 21:59:52 +0800

    美全达

--- app/Http/Controllers/Api/Waimai/MeiTuanSanFang/OrderController.php
+++ app/Http/Controllers/Api/Waimai/MeiTuanSanFang/OrderController.php
@@ -553,7 +553,7 @@ class OrderController extends Controller
             $this->log_info("订单号：{$order_id}|订单不存在");
         }
         if ($order_pt = Order::where('order_id', $order_id)->first()) {
-            if ($order_pt->status == 0) {
+            if ($order_pt->status < 20) {
                 $order_pt->status = 75;
                 $order_pt->over_at = date("Y-m-d H:i:s");
                 $order_pt->save();
