0afe0c01358e4334320dbcfa5fe8209f49fb3897
zhangzhen <zhangzhen@yeah.net>
2023-09-01 10:25:49 +0800

    美全达

--- app/Http/Controllers/Delivery/V1/OrderController.php
+++ app/Http/Controllers/Delivery/V1/OrderController.php
@@ -92,10 +92,10 @@ class OrderController extends Controller
         }, 'order' => function ($query) {
             $query->select('id', 'poi_receive','delivery_time', 'estimate_arrival_time', 'status');
         }, 'shop' => function ($query) {
-            $query->select('id', 'shop_name');
+            $query->select('id', 'shop_lng','shop_lat','shop_name');
         }])->select('id','order_id','wm_id','shop_id','wm_poi_name','receiver_name','receiver_phone','receiver_address','receiver_lng','receiver_lat',
             'caution','day_seq','platform','status','created_at', 'ps as logistic_type','push_at','receive_at','take_at','over_at','cancel_at',
-            'courier_name', 'courier_phone','poi_receive','send_at')
+            'courier_name', 'courier_phone','courier_lng','courier_lat','poi_receive','send_at')
             ->where('ignore', 0)
             ->where('created_at', '>', date('Y-m-d H:i:s', strtotime('-2 day')));
         // 判断权限
@@ -226,6 +226,23 @@ class OrderController extends Controller
                     $order->wm_poi_name = $order->shop->shop_name ?? '';
                 }
                 unset($order->order);
+                // 地图坐标
+                $user_location = [ 'type' => 'user', 'lng' => $order->receiver_lng, 'lat' => $order->receiver_lat, 'title' => '' ];
+                $shop_location = [ 'type' => 'shop', 'lng' => $order->shop->shop_lng, 'lat' => $order->shop->shop_lat, 'title' => '' ];
+                $delivery_location = [ 'type' => 'delivery', 'lng' => $order->courier_lng, 'lat' => $order->courier_lat, 'title' => '' ];
+                if ($order->status <= 20) {
+                    $user_location['title'] = '距离门店' . get_distance_title($order->receiver_lng, $order->receiver_lat, $order->shop->shop_lng, $order->shop->shop_lat);
+                    $locations = [$user_location, $shop_location];
+                } elseif ($order->status == 50) {
+                    $delivery_location['title'] = '距离门店' . get_distance_title($order->receiver_lng, $order->receiver_lat, $order->shop->shop_lng, $order->shop->shop_lat);
+                    $locations = [$user_location, $shop_location, $delivery_location];
+                } elseif ($order->status == 60) {
+                    $delivery_location['title'] = '距离顾客' . get_distance_title($order->receiver_lng, $order->receiver_lat, $order->courier_lng, $order->courier_lat);
+                    $locations = [$user_location, $shop_location, $delivery_location];
+                } else {
+                    $locations = [$user_location];
+                }
+                $order->locations = $locations;
                 unset($order->shop);
             }
         }
