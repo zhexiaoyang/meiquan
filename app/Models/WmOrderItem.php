c57a6a164876a74e20e99d8dae47defc79a2067b
zhangzhen <zhangzhen@yeah.net>
2023-09-01 12:49:13 +0800

    美全达

--- app/Models/WmOrderItem.php
+++ app/Models/WmOrderItem.php
@@ -8,7 +8,7 @@ class WmOrderItem extends Model
 {
     protected $fillable = ["order_id","app_food_code","box_num","box_price","food_name","unit","upc","quantity",
         "price","spec","sku_id","food_discount","food_property","food_share_fee","cart_id","mt_tag_id","mt_spu_id",
-        "mt_sku_id","vip_cost","refund_quantity","image"];
+        "mt_sku_id","vip_cost","refund_quantity","image","image_from"];
 
     public $timestamps = false;
 }
