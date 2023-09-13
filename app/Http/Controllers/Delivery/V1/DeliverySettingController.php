2cad24aabcd73fe49fa3a9446f290602ea97d4d7
zhangzhen <zhangzhen@yeah.net>
2023-09-13 10:50:23 +0800

    美全达

--- app/Http/Controllers/Delivery/V1/DeliverySettingController.php
+++ app/Http/Controllers/Delivery/V1/DeliverySettingController.php
@@ -21,7 +21,7 @@ class DeliverySettingController extends Controller
         if (!$shop_id = intval($request->get("shop_id"))) {
             return $this->error("门店不存在");
         }
-        if (!$shop = Shop::select('id', 'user_id')->find($shop_id)) {
+        if (!$shop = Shop::select('id', 'user_id', 'mt_shop_id', 'ele_shop_id')->find($shop_id)) {
             return $this->error("门店不存在");
         }
         $user = $request->user();
@@ -35,6 +35,8 @@ class DeliverySettingController extends Controller
         }
 
         $result = [
+            'auto_mt' => intval(boolval($shop->mt_shop_id)),
+            'auto_ele' => intval(boolval($shop->ele_shop_id)),
             'delay_send' => $setting['delay_send'],
             'delay_send_text' => $setting['delay_send'] . '秒',
             'delay_reset' => $setting['delay_reset'],
@@ -81,7 +83,7 @@ class DeliverySettingController extends Controller
             return $this->error("切换发单类型错误");
         }
         // 判断门店是否存在
-        if (!$shop = Shop::select('id', 'user_id')->find($shop_id)) {
+        if (!$shop = Shop::select('id', 'user_id', 'mt_shop_id', 'ele_shop_id', 'waimai_mt', 'waimai_ele')->find($shop_id)) {
             return $this->error("门店不存在");
         }
         // 获取当前操作用户
@@ -99,8 +101,36 @@ class DeliverySettingController extends Controller
         $setting->delay_send = $delay_send;
         $setting->delay_reset = $delay_reset;
         $setting->type = $type;
-
         $setting->save();
+        // 自动发单设置
+        $save_status = false;
+        $auto_mt = $request->get('auto_mt');
+        $auto_ele = $request->get('auto_ele');
+        if (!is_null($auto_mt)) {
+            $auto_mt = intval($auto_mt);
+            if ($auto_mt === 0 && $shop->mt_shop_id) {
+                $save_status = true;
+                $shop->mt_shop_id = '';
+            }
+            if ($auto_mt === 1 && !$shop->mt_shop_id) {
+                $save_status = true;
+                $shop->mt_shop_id = $shop->waimai_mt;
+            }
+        }
+        if (!is_null($auto_ele)) {
+            $auto_ele = intval($auto_ele);
+            if ($auto_ele === 0 && $shop->ele_shop_id) {
+                $save_status = true;
+                $shop->ele_shop_id = '';
+            }
+            if ($auto_ele === 1 && !$shop->ele_shop_id) {
+                $save_status = true;
+                $shop->ele_shop_id = $shop->waimai_ele;
+            }
+        }
+        if ($save_status) {
+            $shop->save();
+        }
 
         return $this->success();
     }
