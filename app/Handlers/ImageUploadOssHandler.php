70da076eacd751c72864d2bdd3121cca5f17a63c
zhangzhen <zhangzhen@yeah.net>
2023-09-25 16:12:37 +0800

    美全达

---
+++ app/Handlers/ImageUploadOssHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Handlers;
+
+use  Illuminate\Support\Str;
+
+class ImageUploadOssHandler
+{
+
+    // 只允许以下后缀名的图片文件上传
+    protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
+
+    public function save($file, $folder, $file_prefix)
+    {
+        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
+
+        // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+        // 值如：1_1493521050_7BVc9v9ujP.png
+        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+
+        // 如果上传的不是图片将终止操作
+        if ( ! in_array($extension, $this->allowed_ext)) {
+            return false;
+        }
+
+        // 将图片移动到我们的目标存储路径中
+        $url = '';
+        $oss = app('oss');
+        $res = $oss->uploadFile('peisong-auth', $folder_name . '/' . $filename, $file);
+        if (isset($res['info']['url'])) {
+            $url = "https://img.meiquanda.com/" . $folder_name . '/' . $filename;
+        }
+
+        return $url;
+    }
+}
