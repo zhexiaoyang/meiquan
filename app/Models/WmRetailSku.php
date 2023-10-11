baa4ac42106f51a643f4d65d979988ff951320dd
zhangzhen <zhangzhen@yeah.net>
2023-10-11 11:41:06 +0800

    美全达

--- app/Models/WmRetailSku.php
+++ app/Models/WmRetailSku.php
@@ -8,4 +8,17 @@ class WmRetailSku extends Model
 {
     protected $fillable = ['retail_id','shop_id','sku_id','name','category','second_category','cover','upc','brand',
         'spec','price','down_price','guidance_price','gpm','down_gpm','stock','sequence','mt_status','online_mt'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::saving(function ($model) {
+            if ($model->price > 0) {
+                $model->gpm = ($model->price - $model->guidance_price) / $model->price * 100;
+            }
+            if ($model->down_price > 0) {
+                $model->down_gpm = ($model->down_price - $model->guidance_price) / $model->down_price * 100;
+            }
+        });
+    }
 }
