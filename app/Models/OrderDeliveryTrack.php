406825aa1f8148268405b8f260e3dabe012db5fa
zhangzhen <zhangzhen@yeah.net>
2023-10-17 15:14:44 +0800

    美全达

--- app/Models/OrderDeliveryTrack.php
+++ app/Models/OrderDeliveryTrack.php
@@ -10,12 +10,14 @@ class OrderDeliveryTrack extends Model
     const TRACK_STATUS_WAITING = '等待骑手接单';
     const TRACK_STATUS_RECEIVING = '接单成功';
     const TRACK_STATUS_PICKING = '已到店';
+    const TRACK_STATUS_TRANSFER = '已转单';
     const TRACK_STATUS_DELIVERING = '配送中';
     const TRACK_STATUS_FINISH = '配送完成';
     const TRACK_STATUS_RETURN = '商家退回签收';
     const TRACK_STATUS_CANCEL = '配送已取消';
 
     const TRACK_DESCRIPTION_RECEIVING = '配送员已接单，正赶往商家';
+    const TRACK_DESCRIPTION_TRANSFER = '配送员已更换';
     const TRACK_DESCRIPTION_PICKING = '配送员已就位，请出货';
     const TRACK_DESCRIPTION_DELIVERING = '配送员已取货，正赶往目的地';
     const TRACK_DESCRIPTION_FINISH = '客户已收到商品';
