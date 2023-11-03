99a07a8ffba7740f1791bd2de1af4273bf540e89
zhangzhen <zhangzhen@yeah.net>
2023-11-03 22:06:51 +0800

    美全达

--- app/Jobs/SendOrderToErp.php
+++ app/Jobs/SendOrderToErp.php
@@ -38,54 +38,54 @@ class SendOrderToErp implements ShouldQueue
      */
     public function handle()
     {
-        $data = $this->data;
-        $products = json_decode(urldecode($data['detail']), true);
-
-        $http = new Client();
-        $detail = [];
-        if (!empty($products)) {
-            foreach ($products as $product) {
-                $detail[] = [
-                    "app_food_code" => $product['app_food_code'],
-                    "food_name" => $product['food_name'],
-                    "upc" => $product['upc'],
-                    "price" => (float) $product['price'],
-                    "quantity" => $product['quantity'],
-                    "unit" => $product['unit'],
-                    "spec" => $product['spec'],
-                ];
-            }
-        }
-        $data = [
-            "shop_id" => $this->shop_id,
-            "order_id" => $data['wm_order_id_view'],
-            "wm_order_id_view" => $data['wm_order_id_view'],
-            "recipient_address" => urldecode($data['recipient_address']),
-            "recipient_name" => urldecode($data['recipient_name']) ?? "无名客人",
-            "recipient_phone" => $data['recipient_phone'],
-            "shipping_fee" => (float) $data['shipping_fee'],
-            "total" => (float) $data['total'],
-            "original_price" => (float) $data['original_price'],
-            "caution" => urldecode($data['caution']),
-            "status" => $data['status'],
-            "invoice_title" => $data['invoice_title'] ?? '',
-            "delivery_time" => $data['delivery_time'],
-            "latitude" => (float) $data['latitude'],
-            "longitude" => (float) $data['longitude'],
-            "day_seq" => $data['day_seq'],
-            "detail" => $detail
-        ];
-        $params = [
-            "service_key" => "HXFW_362",
-            "hx_parama" => $data
-        ];
-        try {
-            \Log::info("海协ERP推送订单", $params);
-            $response = $http->post("http://hxfwgw.drugwebcn.com/gateway/apiEntranceAction!apiEntrance.do", [RequestOptions::JSON => $params]);
-            $result = json_decode($response->getBody(), true);
-            \Log::info("海协ERP推送订单-返回", [$result]);
-        } catch (\Exception $exception) {
-            \Log::info("海协ERP推送订单-失败", [$exception->getMessage()]);
-        }
+        // $data = $this->data;
+        // $products = json_decode(urldecode($data['detail']), true);
+        //
+        // $http = new Client();
+        // $detail = [];
+        // if (!empty($products)) {
+        //     foreach ($products as $product) {
+        //         $detail[] = [
+        //             "app_food_code" => $product['app_food_code'],
+        //             "food_name" => $product['food_name'],
+        //             "upc" => $product['upc'],
+        //             "price" => (float) $product['price'],
+        //             "quantity" => $product['quantity'],
+        //             "unit" => $product['unit'],
+        //             "spec" => $product['spec'],
+        //         ];
+        //     }
+        // }
+        // $data = [
+        //     "shop_id" => $this->shop_id,
+        //     "order_id" => $data['wm_order_id_view'],
+        //     "wm_order_id_view" => $data['wm_order_id_view'],
+        //     "recipient_address" => urldecode($data['recipient_address']),
+        //     "recipient_name" => urldecode($data['recipient_name']) ?? "无名客人",
+        //     "recipient_phone" => $data['recipient_phone'],
+        //     "shipping_fee" => (float) $data['shipping_fee'],
+        //     "total" => (float) $data['total'],
+        //     "original_price" => (float) $data['original_price'],
+        //     "caution" => urldecode($data['caution']),
+        //     "status" => $data['status'],
+        //     "invoice_title" => $data['invoice_title'] ?? '',
+        //     "delivery_time" => $data['delivery_time'],
+        //     "latitude" => (float) $data['latitude'],
+        //     "longitude" => (float) $data['longitude'],
+        //     "day_seq" => $data['day_seq'],
+        //     "detail" => $detail
+        // ];
+        // $params = [
+        //     "service_key" => "HXFW_362",
+        //     "hx_parama" => $data
+        // ];
+        // try {
+        //     \Log::info("海协ERP推送订单", $params);
+        //     $response = $http->post("http://hxfwgw.drugwebcn.com/gateway/apiEntranceAction!apiEntrance.do", [RequestOptions::JSON => $params]);
+        //     $result = json_decode($response->getBody(), true);
+        //     \Log::info("海协ERP推送订单-返回", [$result]);
+        // } catch (\Exception $exception) {
+        //     \Log::info("海协ERP推送订单-失败", [$exception->getMessage()]);
+        // }
     }
 }
