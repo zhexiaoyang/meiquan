670694ff56225bd00d6974b29b049f9f423b2b98
zhangzhen <zhangzhen@yeah.net>
2023-11-11 15:44:32 +0800

    美全达

---
+++ app/Models/ShopRestLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShopRestLog extends Model
+{
+    protected $fillable = ['shop_id','shop_name','wm_shop_name','type','status','error'];
+}
