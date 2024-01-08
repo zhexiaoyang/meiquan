62d9235b75ceb7abfacc204401550b8d46685b86
zhangzhen <zhangzhen@yeah.net>
2024-01-08 10:38:55 +0800

    美全达

--- app/Http/Controllers/Delivery/V1/OrderController.php
+++ app/Http/Controllers/Delivery/V1/OrderController.php
@@ -42,7 +42,7 @@ class OrderController extends Controller
         $shop_id = $request->get('shop_id', '');
         $order_where = [['ignore', '=', 0], ['created_at', '>', date('Y-m-d H:i:s', strtotime('-2 day'))],];
         // $wm_order_where = [['created_at', '>', date('Y-m-d H:i:s', strtotime('-1 day'))],];
-        $refund_order_where = [['refund_at', '>=', date('Y-m-d')]];
+        // $refund_order_where = [['refund_at', '>=', date('Y-m-d')]];
         $finish_order_where = [['over_at', '>', date('Y-m-d')], ['status', '>=', 70], ['status', '<=', 75]];
         // $order_where[] = ['shop_id', 'in', $request->user()->shops()->pluck('id')->toArray()];
         // $wm_order_where[] = ['shop_id', 'in', $request->user()->shops()->pluck('id')->toArray()];
@@ -72,7 +72,9 @@ class OrderController extends Controller
             'receiving' => Order::select('id')->where($order_where)->where('status', 50)->count(),
             'delivering' => Order::select('id')->where($order_where)->where('status', 60)->count(),
             'exceptional' => Order::select('id')->where($order_where)->whereIn('status', [10, 5])->count(),
-            'refund' => WmOrder::select('id')->where($refund_order_where)->where('status', 30)->count(),
+            'refund' => WmOrder::select('id')->where(function ($query) {
+                $query->where('refund_at', '>=', date('Y-m-d'))->orWhere('cancel_at', '>=', date('Y-m-d'));
+            })->where('status', 30)->count(),
             'remind' => Order::select('id')->where($order_where)->where('status', '>', 70)->where('remind_num', '>', 0)->count(),
             'finish' => Order::select('id')->where($finish_order_where)->count(),
         ];
@@ -133,7 +135,9 @@ class OrderController extends Controller
         } elseif ($status === 60) {
             // $query->where('status', 20);
             $query->whereHas('order', function ($query) {
-                $query->where('status', 30);
+                $query->where('status', 30)->where(function ($query) {
+                    $query->where('refund_at', '>=', date('Y-m-d'))->orWhere('cancel_at', '>=', date('Y-m-d'));
+                });
             })->where('created_at', '>', date('Y-m-d H:i:s', strtotime('-1 day')));;
         } elseif ($status === 70) {
             $query->where('status', '<', 70)->where('remind_num', '>', 0);
